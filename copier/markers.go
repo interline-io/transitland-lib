d7403ca4daad46d553eaf63d9ed36f0445ced5d1
Ian Rees <ian@ianrees.net>
2019-06-01 15:43:37 -0700

    Remove VisitAndMark from interface

--- copier/markers.go
+++ copier/markers.go
@@ -1,10 +1,7 @@
 package copier
 
-import "github.com/interline-io/gotransit"
-
 // Marker visits and marks entities.
 type Marker interface {
-	VisitAndMark(gotransit.Reader) error
 	IsMarked(string, string) bool
 	IsVisited(string, string) bool
 }
