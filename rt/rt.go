0aaa80acf087d356b337d9847811e8d648295061
Ian Rees <ian@ianrees.net>
2019-08-02 02:21:12 -0700

    Adding readmsg tests

--- rt/rt.go
+++ rt/rt.go
@@ -1,43 +1,20 @@
 package rt
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 
 	"github.com/golang/protobuf/proto"
 	pb "github.com/interline-io/gotransit/rt/transit_realtime"
 )
 
-func readmsg(filename string) error {
+func readmsg(filename string) (pb.FeedMessage, error) {
+	msg := pb.FeedMessage{}
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return err
+		return msg, err
 	}
-	_ = data
-	fmt.Println(len(data))
-	msg := pb.FeedMessage{}
-	err = proto.Unmarshal(data, &msg)
-	if err != nil {
-		panic(err)
+	if err := proto.Unmarshal(data, &msg); err != nil {
+		return msg, err
 	}
-	z, _ := json.Marshal(msg)
-	fmt.Printf("%s\n", z)
-	// fmt.Printf("FeedHeader: %#v\n", msg.Header)
-	// for _, m := range msg.Entity {
-	// 	fmt.Printf("\tFeedMessage: %#v\n", m)
-	// 	if m.TripUpdate != nil {
-	// 		z, _ := json.Marshal(m.TripUpdate)
-	// 		fmt.Printf("\t\tTripUpdate: %s\n", z)
-	// 	}
-	// 	if m.Vehicle != nil {
-	// 		z, _ := json.Marshal(m.Vehicle)
-	// 		fmt.Printf("\t\tVehicle: %s\n", z)
-	// 	}
-	// 	if m.Alert != nil {
-	// 		z, _ := json.Marshal(m.Alert)
-	// 		fmt.Printf("\t\tAlert: %s\n", z)
-	// 	}
-	// }
-	return nil
+	return msg, nil
 }
--- rt/rt_test.go
+++ rt/rt_test.go
@@ -1,10 +1,46 @@
 package rt
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_readmsg(t *testing.T) {
-	err := readmsg("../testdata/rt/example.pbf")
+	msg, err := readmsg("../testdata/rt/example.pbf")
 	if err != nil {
 		t.Error(err)
+	}
+	t.Run("Timestamp", func(t *testing.T) {
+		exp := uint64(1559008978)
+		if msg.Header.Timestamp != nil && uint64(*msg.Header.Timestamp) != exp {
+			got := uint64(*msg.Header.Timestamp)
+			t.Errorf("got %d expect %d", got, exp)
+		}
+	})
+	t.Run("EntityCount", func(t *testing.T) {
+		exp := 26
+		if got := len(msg.Entity); got != exp {
+			t.Errorf("got %d entities, expect %d", got, exp)
+		}
+	})
+	t.Run("Entity", func(t *testing.T) {
+		pbents := msg.Entity
+		if len(pbents) == 0 {
+			t.Error("no message entities")
+			return
+		}
+		ent := pbents[0]
+		exp := "2211905WKDY"
+		if ent.Id != nil && *ent.Id != exp {
+			t.Errorf("got '%s' expect '%s'", *ent.Id, exp)
+		}
+	})
+	// z, _ := json.Marshal(msg)
+	// fmt.Printf("%s\n", z)
+}
+
+func Test_readmsg_error(t *testing.T) {
+	_, err := readmsg("../testdata/example.zip")
+	if err == nil {
+		t.Errorf("got no error, expected illegal tag")
 	}
 }
