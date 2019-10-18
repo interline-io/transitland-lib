e2df287526c19869dffd8e0544dfdd04f83abb6c
Ian Rees <ian@ianrees.net>
2019-10-18 14:49:31 -0700

    SQL logging

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,27 +1,54 @@
 package log
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gookit/color"
+)
+
+// Level values
+const (
+	FATAL    = 100
+	CRITICAL = 50
+	ERROR    = 40
+	WARNING  = 30
+	INFO     = 20
+	QUERY    = 11
+	DEBUG    = 10
+	TRACE    = 5
 )
 
 // LEVELSTRINGS provides log level aliases.
 var LEVELSTRINGS = map[string]int{
-	"critical": 50,
-	"error":    40,
-	"warning":  30,
-	"info":     20,
-	"debug":    10,
-	"trace":    5,
+	"CRITICAL": CRITICAL,
+	"ERROR":    ERROR,
+	"WARNING":  WARNING,
+	"INFO":     INFO,
+	"DEBUG":    DEBUG,
+	"TRACE":    TRACE,
+	"QUERY":    QUERY,
+}
+
+// STRINGLEVEL is the reverse mapping
+var STRINGLEVEL = map[int]string{}
+
+func init() {
+	for k, v := range LEVELSTRINGS {
+		STRINGLEVEL[v] = k
+	}
 }
 
 // Level is the log level.
-var Level = 10
+var Level = DEBUG
 
 // Printf is the same as Info.
 func Printf(fmt string, a ...interface{}) {
-	logLog(20, fmt, a...)
+	logLog(INFO, fmt, a...)
 }
 
 // Println is for compatibility.
@@ -31,28 +58,79 @@ func Println(a ...interface{}) {
 
 // Info for regular messages.
 func Info(fmt string, a ...interface{}) {
-	logLog(20, fmt, a...)
+	logLog(INFO, fmt, a...)
 }
 
 // Debug for debugging messages.
 func Debug(fmt string, a ...interface{}) {
-	logLog(10, fmt, a...)
+	logLog(DEBUG, fmt, a...)
 }
 
 // Trace for really deep debugging.
 func Trace(fmt string, a ...interface{}) {
-	logLog(5, fmt, a...)
+	logLog(TRACE, fmt, a...)
 }
 
 // Fatal for fatal, unrecoverable errors.
 func Fatal(fmta string, a ...interface{}) {
-	logLog(100, fmta, a...)
+	logLog(FATAL, fmta, a...)
 	panic(fmt.Sprintf(fmta, a...))
 }
 
+// canToSQL is the squirrel interface
+type canToSQL interface {
+	ToSql() (string, []interface{}, error)
+}
+
+type canValue interface {
+	Value() (driver.Value, error)
+}
+
+type qval struct {
+	Name  string
+	Value interface{}
+}
+
+func (q qval) String() string {
+	s := ""
+	if a, ok := q.Value.(canValue); ok {
+		z, _ := a.Value()
+		s = fmt.Sprintf("%s", z)
+	} else {
+		s = fmt.Sprintf("%s", q.Value)
+	}
+	return fmt.Sprintf("{%s:%s}", q.Name, s)
+}
+
+// Query for logging database queries.
+func Query(qstr string, a ...interface{}) {
+	sts := []string{}
+	for i, val := range a {
+		q := qval{strconv.Itoa(i + 1), val}
+		sts = append(sts, q.String())
+	}
+	fmta := qstr
+	logLog(QUERY, color.Blue.Render(fmta)+" -- "+color.Gray.Render(strings.Join(sts, " ")))
+}
+
+// Sq for logging Squirrel Queries; avoids ToSql evaluation unless log level.
+func Sq(q canToSQL) {
+	level := DEBUG
+	if level < Level {
+		return
+	}
+	qstr, qargs, err := q.ToSql()
+	if err != nil {
+		Query("error building query: %s", err)
+		return
+	}
+	Query(qstr, qargs...)
+}
+
 func logLog(level int, fmt string, a ...interface{}) {
+	strlevel, _ := STRINGLEVEL[level]
 	if level >= Level {
-		log.Printf(fmt, a...)
+		log.Printf("["+strlevel+"] "+fmt, a...)
 	}
 }
 
@@ -63,7 +141,7 @@ func SetLevel(level int) {
 
 // SetLevelString uses a string alias to set the log level.
 func SetLevelString(lstr string) {
-	lvalue, ok := LEVELSTRINGS[lstr]
+	lvalue, ok := LEVELSTRINGS[strings.ToUpper(lstr)]
 	if !ok {
 		lvalue = 20
 	}
