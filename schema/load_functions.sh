fabfa5a96dcff11dcaf032aa84df6c250be207c0
Ian Rees <ian@ianrees.net>
2020-02-03 18:29:38 -0800

    Move to schema repo

--- schema/load_functions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-psql -f activate_feed_version.pgsql
-psql -f after_feed_version_import.pgsql
-psql -f assign_agency_places.pgsql
-psql -f calculate_route_headways.pgsql
-psql -f services_on_date.pgsql
-psql -f unimport_feed_version.pgsql
-psql -f views.pgsql
