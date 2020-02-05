b46ff3284a6d1871d298dedd5976e2ed22890949
Ian Rees <ian@ianrees.net>
2020-02-04 21:19:59 -0800

    DMFR Secret and tests

---
+++ dmfr/secret.go
@@ -0,0 +1,69 @@
+package dmfr
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+// Secrets loads a JSON file of secrets
+type Secrets []Secret
+
+func (s *Secrets) Load(filename string) error {
+	bv, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(bv, s)
+	return nil
+}
+
+func (s Secrets) MatchFilename(filename string) (Secret, error) {
+	if len(filename) == 0 {
+		return Secret{}, errors.New("no filename provided")
+	}
+	found := Secret{}
+	count := 0
+	for _, secret := range s {
+		if secret.Filename == filename {
+			count++
+			found = secret
+		}
+	}
+	if count == 0 {
+		return Secret{}, errors.New("no results")
+	} else if count > 1 {
+		return Secret{}, fmt.Errorf("ambiguous results; %d matches", count)
+	}
+	return found, nil
+}
+
+func (s Secrets) MatchFeed(feedid string) (Secret, error) {
+	if len(feedid) == 0 {
+		return Secret{}, errors.New("no feedid provided")
+	}
+	found := Secret{}
+	count := 0
+	for _, secret := range s {
+		if secret.Feed == feedid {
+			count++
+			found = secret
+		}
+	}
+	if count == 0 {
+		return Secret{}, errors.New("no results")
+	} else if count > 1 {
+		return Secret{}, fmt.Errorf("ambiguous results; %d matches", count)
+	}
+	return found, nil
+}
+
+// Secret .
+type Secret struct {
+	Key      string `json:"key"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Feed     string `json:"feed"`
+	Filename string `json:"filename"`
+}
