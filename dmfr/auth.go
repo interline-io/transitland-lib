44b40e79e4d7b79bd28caeaba98e35fbe25b8bb4
Ian Rees <ian@ianrees.net>
2020-02-05 16:25:29 -0800

    Auth debug logging

--- dmfr/auth.go
+++ dmfr/auth.go
@@ -24,8 +24,11 @@ func AuthenticatedRequest(address string, secret Secret, auth gotransit.FeedAuth
 		}
 		v.Set(auth.ParamName, secret.Key)
 		u.RawQuery = v.Encode()
+		log.Debug("Using query_param authentication: %s = %s", auth.ParamName, secret.Key)
 	} else if auth.Type == "path_segment" {
+		p := u.Path
 		u.Path = strings.ReplaceAll(u.Path, "{}", secret.Key)
+		log.Debug("Using path_segment authentication: %s -> %s", p, u.Path)
 	}
 	ustr := u.String()
 	// Get the temporary file and full path
@@ -43,15 +46,17 @@ func AuthenticatedRequest(address string, secret Secret, auth gotransit.FeedAuth
 	}
 	if auth.Type == "basic_auth" {
 		req.SetBasicAuth(secret.Username, secret.Password)
+		log.Debug("Using basic_auth authentication: %s:%s", secret.Username, secret.Password)
 	} else if auth.Type == "header" {
+		log.Debug("Using header authentication: %s = %s", auth.ParamName, secret.Key)
 		req.Header.Add(auth.ParamName, secret.Key)
 	}
+	log.Debug("AuthorizedRequest downloading %s -> %s", address, tmpfilepath)
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	// Write the body to file
 	_, err = io.Copy(tmpfile, resp.Body)
-	log.Debug("Downloaded %s to %s", ustr, tmpfilepath)
 	return tmpfilepath, nil
 }
