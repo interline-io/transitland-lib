259c74dd3e1c0c9685b3990026207dc62625b666
Ian Rees <ian@ianrees.net>
2020-06-22 19:07:12 -0700

    Make validator helper methods part of public api

---
+++ validator/validate.go
@@ -0,0 +1,75 @@
+package validator
+
+import (
+	"strings"
+)
+
+/* Validation Helpers */
+
+// IsValidLang check is valid language
+func IsValidLang(value string) bool {
+	if len(value) == 0 {
+		return true
+	}
+	// Only check the prefix code
+	code := strings.Split(value, "-")
+	_, ok := langs[strings.ToLower(code[0])]
+	return ok
+}
+
+// IsValidCurrency check is valid currency
+func IsValidCurrency(value string) bool {
+	if len(value) == 0 {
+		return true
+	}
+	_, ok := currencies[strings.ToLower(value)]
+	return ok
+}
+
+// IsValidTimezone check is valid timezone
+func IsValidTimezone(value string) bool {
+	if len(value) == 0 {
+		return true
+	}
+	_, ok := timezones[strings.ToLower(value)]
+	return ok
+}
+
+// IsValidEmail check if valid email
+func IsValidEmail(email string) bool {
+	if strings.Contains(email, "@") {
+		return true
+	} else if len(email) == 0 {
+		return true
+	}
+	return false
+}
+
+// IsValidColor check is valid color
+func IsValidColor(color string) bool {
+	// todo: hex validation?
+	if len(color) == 0 {
+		return true
+	} else if len(color) == 7 && strings.HasPrefix(color, "#") {
+		return true
+	} else if len(color) == 6 {
+		return true
+	}
+	return false
+}
+
+// IsValidURL check is valid url
+func IsValidURL(url string) bool {
+	// todo: full validation?
+	if strings.HasPrefix(url, "http://") {
+		return true
+	} else if strings.HasPrefix(url, "https://") {
+		return true
+	} else if strings.Contains(url, ".") {
+		// allow bare hosts, e.g. "example.com"
+		return true
+	} else if len(url) == 0 {
+		return true
+	}
+	return false
+}
