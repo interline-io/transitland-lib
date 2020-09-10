dd56c25ae47a6cef4849b459cf3e6f75a4b84094
Ian Rees <ian@ianrees.net>
2020-09-10 14:58:36 -0700

    version bump

--- tl/tl.go
+++ tl/tl.go
@@ -1,7 +1,7 @@
 package tl
 
 // VERSION is the current software version.
-var VERSION = "v0.6.0"
+var VERSION = "v0.6.1"
 
 // GTFSVERSION is the commit for the spec reference.md file.
 var GTFSVERSION = "fec428e9023a8dabb1a23cbd56ba607fe5e3e96c"
