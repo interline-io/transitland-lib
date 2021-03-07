e712bacb0828ee6d045e6a23c9847caf71e1b82e
Ian Rees <ian@ianrees.net>
2021-03-06 19:46:40 -0800

    Validator Options and Result types

--- cmd/transitland/validate.go
+++ cmd/transitland/validate.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
+	"os"
 
 	"github.com/interline-io/transitland-lib/ext"
 	"github.com/interline-io/transitland-lib/internal/cli"
@@ -12,6 +13,8 @@ import (
 
 // validateCommand
 type validateCommand struct {
+	Options            validator.Options
+	OutputFile         string
 	validateExtensions cli.ArrayFlags
 }
 
@@ -22,25 +25,44 @@ func (cmd *validateCommand) Run(args []string) error {
 		fl.PrintDefaults()
 	}
 	fl.Var(&cmd.validateExtensions, "ext", "Include GTFS Extension")
+	fl.StringVar(&cmd.OutputFile, "o", "", "Write validation report as JSON to file")
+	fl.BoolVar(&cmd.Options.BestPractices, "best-practices", false, "Include Best Practices validations")
 	err := fl.Parse(args)
 	if err != nil || fl.NArg() < 1 {
 		fl.Usage()
 		log.Exit("Requires input reader")
 	}
-	//
 	reader := ext.MustGetReader(fl.Arg(0))
 	defer reader.Close()
-	v, err := validator.NewValidator(reader)
+	v, err := validator.NewValidator(reader, cmd.Options)
 	if err != nil {
 		return err
 	}
-	for _, extName := range cmd.validateExtensions {
-		e, err := ext.GetExtension(extName)
+	// TODO
+	// for _, extName := range cmd.validateExtensions {
+	// 	e, err := ext.GetExtension(extName)
+	// 	if err != nil {
+	// 		return fmt.Errorf("No extension for: %s", extName)
+	// 	}
+	// 	v.Copier.AddExtension(e)
+	// }
+	result, _ := v.Validate()
+	result.DisplayErrors()
+	result.DisplayWarnings()
+	result.DisplaySummary()
+
+	// Write output
+	if cmd.OutputFile != "" {
+		f, err := os.Create(cmd.OutputFile)
 		if err != nil {
-			return fmt.Errorf("No extension for: %s", extName)
+			panic(err)
 		}
-		v.Copier.AddExtension(e)
+		b, err := json.Marshal(result)
+		if err != nil {
+			panic(err)
+		}
+		f.Write(b)
+		f.Close()
 	}
-	v.Validate()
 	return nil
 }
