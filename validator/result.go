4149405fa589f7d670a0cba2716924e1586d6b50
Ian Rees <ian@ianrees.net>
2021-03-06 19:55:31 -0800

    Extended validation reports

--- validator/result.go
+++ validator/result.go
@@ -1,12 +1,25 @@
 package validator
 
 import (
+	"time"
+
 	"github.com/interline-io/transitland-lib/copier"
+	"github.com/interline-io/transitland-lib/dmfr"
+	"github.com/interline-io/transitland-lib/tl"
 )
 
 // Result contains a validation report result,
 type Result struct {
-	copier.Result        // add to copier result:
-	Success       bool   `json:"success"`
-	FailureReason string `json:"failure_reason"`
+	copier.Result                                       // add to copier result:
+	Success              bool                           `json:"success"`
+	FailureReason        string                         `json:"failure_reason"`
+	SHA1                 string                         `json:"sha1"`
+	EarliestCalendarDate time.Time                      `json:"earliest_calendar_date"`
+	LatestCalendarDate   time.Time                      `json:"latest_calendar_date"`
+	Agencies             []tl.Agency                    `json:"agencies"`
+	Routes               []tl.Route                     `json:"routes"`
+	Stops                []tl.Stop                      `json:"stops"`
+	FeedInfos            []tl.FeedInfo                  `json:"feed_infos"`
+	Files                []dmfr.FeedVersionFileInfo     `json:"files"`
+	ServiceLevels        []dmfr.FeedVersionServiceLevel `json:"service_levels"`
 }
--- validator/validator.go
+++ validator/validator.go
@@ -1,10 +1,16 @@
 package validator
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/interline-io/transitland-lib/copier"
+	"github.com/interline-io/transitland-lib/dmfr"
 	"github.com/interline-io/transitland-lib/rules"
 	"github.com/interline-io/transitland-lib/tl"
 	"github.com/interline-io/transitland-lib/tl/causes"
+	"github.com/interline-io/transitland-lib/tlcsv"
+	"github.com/twpayne/go-geom"
 )
 
 type errorWithContext interface {
@@ -56,9 +62,41 @@ func NewValidator(reader tl.Reader, options Options) (*Validator, error) {
 
 // Validate performs a basic validation, as well as optional extended reports.
 func (v *Validator) Validate() (*Result, error) {
+	t := time.Now()
+	reader := v.Reader
 	result := Result{}
+	result.EarliestCalendarDate = time.Now()
+	result.LatestCalendarDate = time.Now()
+
+	// Check file infos first, so we exit early if a file exceeds the row limit.
+	if reader2, ok := reader.(*tlcsv.Reader); ok {
+		fmt.Println("file infos")
+		fvfis, err := dmfr.NewFeedVersionFileInfosFromReader(reader2)
+		if err != nil {
+			result.FailureReason = fmt.Sprintf("Could not read basic CSV data from file: %s", err.Error())
+			return &result, nil
+		}
+		result.Files = fvfis
+		// Maximum file limits
+		if v.Options.CheckFileLimits {
+			for _, fvfi := range fvfis {
+				if maxRows, ok := defaultMaxFileRows[fvfi.Name]; ok && fvfi.Rows > maxRows {
+					result.FailureReason = fmt.Sprintf(
+						"File '%s' exceeded maximum size; got %d rows, max allowed %d rows",
+						fvfi.Name,
+						fvfi.Rows,
+						maxRows,
+					)
+					return &result, nil
+				}
+			}
+		}
+		fmt.Println("done:", float64(time.Now().UnixNano()-t.UnixNano())/1e9)
+		t = time.Now()
+	}
 
 	// Main validation
+	t = time.Now()
 	w := emptyWriter{}
 	w.Open()
 	copier := copier.NewCopier(v.Reader, &w, v.Options.Options)
@@ -80,7 +118,76 @@ func (v *Validator) Validate() (*Result, error) {
 		result.FailureReason = "Failed to validate feed"
 		return &result, nil
 	}
+	fmt.Println("done:", float64(time.Now().UnixNano()-t.UnixNano())/1e9)
+	t = time.Now()
 
+	// Service levels
+	if v.Options.IncludeServiceLevels {
+		fmt.Println("service levels")
+		fvsls, err := dmfr.NewFeedVersionServiceInfosFromReader(reader)
+		if err != nil {
+			result.FailureReason = fmt.Sprintf("Could not calculate service levels: %s", err.Error())
+			return &result, nil
+		}
+		for i, fvsl := range fvsls {
+			if i > v.Options.IncludeEntitiesLimit {
+				break
+			}
+			// TODO: deal with routes later.
+			// For now only copy feed level service levels...
+			if !fvsl.RouteID.Valid {
+				continue
+			}
+			result.ServiceLevels = append(result.ServiceLevels, fvsl)
+		}
+		fmt.Println("done:", float64(time.Now().UnixNano()-t.UnixNano())/1e9)
+		t = time.Now()
+	}
+
+	routeShapes := map[string]*geom.MultiLineString{}
+	if v.Options.IncludeRouteGeometries {
+		// Build shapes...
+		fmt.Println("building shapes")
+		routeShapes = buildRouteShapes(reader)
+		fmt.Println("done:", float64(time.Now().UnixNano()-t.UnixNano())/1e9)
+		t = time.Now()
+	}
+
+	// Include some basic entities in the report
+	if v.Options.IncludeEntities {
+		// Add basic entities
+		fmt.Println("adding basic entities")
+		for ent := range reader.Agencies() {
+			result.Agencies = append(result.Agencies, ent)
+			if len(result.Agencies) >= v.Options.IncludeEntitiesLimit {
+				break
+			}
+		}
+		for ent := range reader.Routes() {
+			ent := ent
+			if s, ok := routeShapes[ent.RouteID]; ok {
+				g := tl.Geometry{Geometry: s, Valid: true}
+				ent.Geometry = g
+			}
+			result.Routes = append(result.Routes, ent)
+			if len(result.Routes) >= v.Options.IncludeEntitiesLimit {
+				break
+			}
+		}
+		for ent := range reader.Stops() {
+			result.Stops = append(result.Stops, ent)
+			if len(result.Stops) >= v.Options.IncludeEntitiesLimit {
+				break
+			}
+		}
+		for ent := range reader.FeedInfos() {
+			result.FeedInfos = append(result.FeedInfos, ent)
+			if len(result.FeedInfos) >= v.Options.IncludeEntitiesLimit {
+				break
+			}
+		}
+		fmt.Println("done:", float64(time.Now().UnixNano()-t.UnixNano())/1e9)
+	}
 	result.Success = true
 	return &result, nil
 }
