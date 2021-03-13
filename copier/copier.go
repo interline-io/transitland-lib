21b9cce03383dde8587441e3e2d33fde14a79594
Ian Rees <ian@ianrees.net>
2021-03-12 17:23:30 -0800

    Clarify  agency_id required/recommended rules

--- copier/copier.go
+++ copier/copier.go
@@ -597,11 +597,8 @@ func (copier *Copier) copyCalendars() error {
 	for _, svc := range svcs {
 		// Skip main Calendar entity if generated and not normalizing service IDs.
 		if svc.Generated && !copier.NormalizeServiceIDs && !copier.SimplifyCalendars {
-			fmt.Println("skip svc:", svc.ServiceID, "generated:", svc.Generated, "normalize:", copier.NormalizeServiceIDs, "simplify:", copier.SimplifyCalendars)
 			copier.SetEntity(&svc.Calendar, svc.ServiceID, svc.ServiceID)
 			continue
-		} else {
-			fmt.Println("write svc:", svc.ServiceID, "generated:", svc.Generated, "normalize:", copier.NormalizeServiceIDs, "simplify:", copier.SimplifyCalendars)
 		}
 		// Validate as Service, with attached exceptions, for better validation.
 		if bt, err = copier.checkBatch(bt, svc); err != nil {
