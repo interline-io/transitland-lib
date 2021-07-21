4e06c6e26dd2735482f699a0c1e1631483a85f9d
Ian Rees <ian@ianrees.net>
2021-07-20 19:13:04 -0700

    sqlite schema updates

--- internal/schema/sqlite.sql
+++ internal/schema/sqlite.sql
@@ -13,8 +13,31 @@ CREATE TABLE IF NOT EXISTS "current_feeds" (
   "associated_feeds" BLOB,
   "feed_namespace_id" varchar(255) NOT NULL,
   "name" varchar(255),
-  "file" varchar(255) NOT NULL
+  "file" varchar(255) NOT NULL,
+  "feed_tags" BLOB
 );
+CREATE INDEX idx_current_feeds_onestop_id ON "current_feeds"(onestop_id);
+
+CREATE TABLE IF NOT EXISTS "current_operators" (
+  "id" integer primary key autoincrement, 
+  "onestop_id" varchar(255) NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "short_name" varchar(255) NOT NULL,
+  "website" varchar(255) NOT NULL,
+  "operator_tags" BLOB,
+  "created_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+  "updated_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+  "deleted_at" datetime
+);
+CREATE INDEX idx_current_operators_onestop_id ON "current_operators"(onestop_id);
+
+CREATE TABLE IF NOT EXISTS "current_operators_in_feed" (
+  "id" integer primary key autoincrement, 
+  "operator_id" integer not null,
+  "feed_id" integer not null,
+  "gtfs_agency_id" varchar(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS "feed_version_gtfs_imports" (
   "id" integer primary key autoincrement, 
   "feed_version_id" integer NOT NULL, 
@@ -34,6 +57,7 @@ CREATE TABLE IF NOT EXISTS "feed_version_gtfs_imports" (
   "warning_count" blob,
   "entity_count" blob
 );
+
 CREATE TABLE IF NOT EXISTS "gtfs_stops" (
   "id" integer primary key autoincrement, 
   "feed_version_id" integer NOT NULL, 
@@ -52,6 +76,10 @@ CREATE TABLE IF NOT EXISTS "gtfs_stops" (
   "level_id" integer,
   "geometry" BLOB NOT NULL
 );
+CREATE INDEX idx_gtfs_stops_stop_id ON "gtfs_stops"(stop_id);
+CREATE INDEX idx_gtfs_stops_parent_station ON "gtfs_stops"(parent_station);
+CREATE INDEX idx_gtfs_stops_feed_version_id ON "gtfs_stops"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_pathways" (
   "id" integer primary key autoincrement, 
   "feed_version_id" integer NOT NULL, 
@@ -70,6 +98,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_pathways" (
   "signposted_as" varchar(255) NOT NULL,
   "reverse_signposted_as" varchar(255) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS "gtfs_levels" (
   "id" integer primary key autoincrement, 
   "feed_version_id" integer NOT NULL, 
@@ -79,6 +108,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_levels" (
   "level_index" real NOT NULL,
   "level_name" varchar(255) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS "gtfs_shapes" (
   "id" integer primary key autoincrement, 
   "feed_version_id" integer NOT NULL, 
@@ -88,6 +118,9 @@ CREATE TABLE IF NOT EXISTS "gtfs_shapes" (
   "generated" bool NOT NULL,
   "geometry" BLOB NOT NULL
 );
+CREATE INDEX idx_gtfs_shapes_shape_id ON "gtfs_shapes"(shape_id);
+CREATE INDEX idx_gtfs_shapes_feed_version_id ON "gtfs_shapes"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "feed_versions" (
   "feed_id" integer, 
   "feed_type" varchar(255) NOT NULL, 
@@ -106,6 +139,46 @@ CREATE TABLE IF NOT EXISTS "feed_versions" (
   "name" varchar(255),
   "description" varchar(255)
 );
+CREATE INDEX idx_feed_versions_sha1 ON "feed_versions"("sha1");
+CREATE INDEX idx_feed_versions_earliest_calendar_date ON "feed_versions"(earliest_calendar_date);
+CREATE INDEX idx_feed_versions_latest_calendar_date ON "feed_versions"(latest_calendar_date);
+CREATE INDEX idx_feed_versions_feed_id ON "feed_versions"(feed_id);
+CREATE INDEX idx_feed_versions_feed_type ON "feed_versions"(feed_type);
+
+CREATE TABLE IF NOT EXISTS "feed_version_file_infos" (
+  "id" integer primary key autoincrement, 
+  "feed_version_id" integer NOT NULL, 
+  "created_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+  "updated_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+  "name" varchar(255) not null,
+  "size" integer not null,
+  "rows" integer not null,
+  "columns" int not null,
+  "sha1" varchar(255) not null,
+  "header" varchar(255) not null,
+  "csv_like" bool not null
+);
+CREATE INDEX idx_feed_version_file_infos_feed_version_id ON "feed_version_file_infos"(feed_version_id);
+
+CREATE TABLE IF NOT EXISTS "feed_version_service_levels" (
+    "id" integer primary key autoincrement,
+    "feed_version_id" integer NOT NULL,
+    "route_id" varchar(255),
+    "start_date" datetime NOT NULL,
+    "end_date" datetime NOT NULL,
+    "agency_name" varchar(255) NOT NULL,
+    "route_short_name" varchar(255) NOT NULL,
+    "route_long_name" varchar(255) NOT NULL,
+    "route_type" integer NOT NULL,
+    "monday" integer NOT NULL,
+    "tuesday" integer NOT NULL,
+    "wednesday" integer NOT NULL,
+    "thursday" integer NOT NULL,
+    "friday" integer NOT NULL,
+    "saturday" integer NOT NULL,
+    "sunday" integer NOT NULL
+);
+CREATE INDEX idx_feed_version_service_levels_feed_version_id ON "feed_version_service_levels"(feed_version_id);
 
 CREATE TABLE IF NOT EXISTS "feed_states" (
     "id" integer primary key autoincrement, 
@@ -123,16 +196,6 @@ CREATE TABLE IF NOT EXISTS "feed_states" (
     "geometry" BLOB
 );
 
-CREATE INDEX idx_feed_versions_sha1 ON "feed_versions"("sha1");
-CREATE INDEX idx_feed_versions_earliest_calendar_date ON "feed_versions"(earliest_calendar_date);
-CREATE INDEX idx_feed_versions_latest_calendar_date ON "feed_versions"(latest_calendar_date);
-CREATE INDEX idx_feed_versions_feed_id ON "feed_versions"(feed_id);
-CREATE INDEX idx_feed_versions_feed_type ON "feed_versions"(feed_type);
-CREATE INDEX idx_gtfs_stops_stop_id ON "gtfs_stops"(stop_id);
-CREATE INDEX idx_gtfs_stops_parent_station ON "gtfs_stops"(parent_station);
-CREATE INDEX idx_gtfs_stops_feed_version_id ON "gtfs_stops"(feed_version_id);
-CREATE INDEX idx_gtfs_shapes_shape_id ON "gtfs_shapes"(shape_id);
-CREATE INDEX idx_gtfs_shapes_feed_version_id ON "gtfs_shapes"(feed_version_id);
 CREATE TABLE IF NOT EXISTS "gtfs_feed_infos" (
   "feed_publisher_name" varchar(255) NOT NULL, 
   "feed_publisher_url" varchar(255) NOT NULL, 
@@ -146,6 +209,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_feed_infos" (
   "updated_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 CREATE INDEX idx_gtfs_feed_infos_feed_version_id ON "gtfs_feed_infos"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_frequencies" (
   "trip_id" int NOT NULL, 
   "start_time" int NOT NULL, 
@@ -159,6 +223,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_frequencies" (
 );
 CREATE INDEX idx_gtfs_frequencies_trip_id ON "gtfs_frequencies"(trip_id);
 CREATE INDEX idx_gtfs_frequencies_feed_version_id ON "gtfs_frequencies"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_trips" (
   "route_id" int NOT NULL, 
   "service_id" int NOT NULL, 
@@ -184,6 +249,7 @@ CREATE INDEX idx_gtfs_trips_trip_id ON "gtfs_trips"(trip_id);
 CREATE INDEX idx_gtfs_trips_shape_id ON "gtfs_trips"(shape_id);
 CREATE INDEX idx_gtfs_trips_stop_pattern_id ON "gtfs_trips"(stop_pattern_id);
 CREATE INDEX idx_gtfs_trips_feed_version_id ON "gtfs_trips"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_agencies" (
   "agency_id" varchar(255) NOT NULL, 
   "agency_name" varchar(255) NOT NULL, 
@@ -200,6 +266,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_agencies" (
 );
 CREATE INDEX idx_gtfs_agencies_agency_id ON "gtfs_agencies"(agency_id);
 CREATE INDEX idx_gtfs_agencies_feed_version_id ON "gtfs_agencies"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_transfers" (
   "from_stop_id" int NOT NULL, 
   "to_stop_id" int NOT NULL, 
@@ -214,6 +281,7 @@ CREATE INDEX idx_gtfs_transfers_transfer_type ON "gtfs_transfers"(transfer_type)
 CREATE INDEX idx_gtfs_transfers_feed_version_id ON "gtfs_transfers"(feed_version_id);
 CREATE INDEX idx_gtfs_transfers_from_stop_id ON "gtfs_transfers"(from_stop_id);
 CREATE INDEX idx_gtfs_transfers_to_stop_id ON "gtfs_transfers"(to_stop_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_calendars" (
   "service_id" varchar(255) NOT NULL, 
   "monday" integer NOT NULL, 
@@ -242,6 +310,7 @@ CREATE INDEX idx_gtfs_calendars_thursday ON "gtfs_calendars"("thursday");
 CREATE INDEX idx_gtfs_calendars_friday ON "gtfs_calendars"("friday");
 CREATE INDEX idx_gtfs_calendars_saturday ON "gtfs_calendars"("saturday");
 CREATE INDEX idx_gtfs_calendars_sunday ON "gtfs_calendars"("sunday");
+
 CREATE TABLE IF NOT EXISTS "gtfs_calendar_dates" (
   "service_id" int NOT NULL, 
   "date" datetime NOT NULL, 
@@ -255,6 +324,7 @@ CREATE INDEX idx_gtfs_calendar_dates_date ON "gtfs_calendar_dates"("date");
 CREATE INDEX idx_gtfs_calendar_dates_exception_type ON "gtfs_calendar_dates"(exception_type);
 CREATE INDEX idx_gtfs_calendar_dates_feed_version_id ON "gtfs_calendar_dates"(feed_version_id);
 CREATE INDEX idx_gtfs_calendar_dates_service_id ON "gtfs_calendar_dates"(service_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_routes" (
   "route_id" varchar(255) NOT NULL, 
   "agency_id" int NOT NULL, 
@@ -275,6 +345,7 @@ CREATE INDEX idx_gtfs_routes_route_id ON "gtfs_routes"(route_id);
 CREATE INDEX idx_gtfs_routes_agency_id ON "gtfs_routes"(agency_id);
 CREATE INDEX idx_gtfs_routes_route_type ON "gtfs_routes"(route_type);
 CREATE INDEX idx_gtfs_routes_feed_version_id ON "gtfs_routes"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_stop_times" (
   "trip_id" int NOT NULL, 
   "arrival_time" int NOT NULL, 
@@ -292,6 +363,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_stop_times" (
 CREATE INDEX idx_stop_times_trip_id ON "gtfs_stop_times"(trip_id);
 CREATE INDEX idx_gtfs_stop_times_stop_id ON "gtfs_stop_times"(stop_id);
 CREATE INDEX idx_gtfs_stop_times_feed_version_id ON "gtfs_stop_times"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_fare_rules" (
   "fare_id" int NOT NULL, 
   "route_id" int, 
@@ -305,6 +377,7 @@ CREATE TABLE IF NOT EXISTS "gtfs_fare_rules" (
 );
 CREATE INDEX idx_gtfs_fare_rules_fare_id ON "gtfs_fare_rules"(fare_id);
 CREATE INDEX idx_gtfs_fare_rules_feed_version_id ON "gtfs_fare_rules"(feed_version_id);
+
 CREATE TABLE IF NOT EXISTS "gtfs_fare_attributes" (
   "fare_id" varchar(255) NOT NULL, 
   "price" real NOT NULL, 
@@ -325,41 +398,3 @@ CREATE INDEX idx_gtfs_fare_attributes_feed_version_id ON "gtfs_fare_attributes"(
 -------------------
 
 
-CREATE TABLE IF NOT EXISTS "feed_version_file_infos" (
-  "id" integer primary key autoincrement, 
-  "feed_version_id" integer NOT NULL, 
-  "created_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
-  "updated_at" datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
-  "name" varchar(255) not null,
-  "size" integer not null,
-  "rows" integer not null,
-  "columns" int not null,
-  "sha1" varchar(255) not null,
-  "header" varchar(255) not null,
-  "csv_like" bool not null
-);
-
-CREATE INDEX idx_feed_version_file_infos_feed_version_id ON "feed_version_file_infos"(feed_version_id);
-
---------------------
-
-CREATE TABLE IF NOT EXISTS "feed_version_service_levels" (
-    "id" integer primary key autoincrement,
-    "feed_version_id" integer NOT NULL,
-    "route_id" varchar(255),
-    "start_date" datetime NOT NULL,
-    "end_date" datetime NOT NULL,
-    "agency_name" varchar(255) NOT NULL,
-    "route_short_name" varchar(255) NOT NULL,
-    "route_long_name" varchar(255) NOT NULL,
-    "route_type" integer NOT NULL,
-    "monday" integer NOT NULL,
-    "tuesday" integer NOT NULL,
-    "wednesday" integer NOT NULL,
-    "thursday" integer NOT NULL,
-    "friday" integer NOT NULL,
-    "saturday" integer NOT NULL,
-    "sunday" integer NOT NULL
-);
-
-CREATE INDEX idx_feed_version_service_levels_feed_version_id ON "feed_version_service_levels"(feed_version_id);
