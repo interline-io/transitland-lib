b82fb3c542baf8dd33d281a1656a88b56911cab9
Drew Dara-Abrams <drew@interline.io>
2021-11-02 12:17:03 -0700

    expose commit hash for the GTFS Realtime proto definition; also update the proto file to latest version

--- tl/tl.go
+++ tl/tl.go
@@ -5,3 +5,6 @@ var VERSION = "v0.8.7"
 
 // GTFSVERSION is the commit for the spec reference.md file.
 var GTFSVERSION = "fec428e9023a8dabb1a23cbd56ba607fe5e3e96c"
+
+// GTFSRTVERSION is the commit for the gtfs-realtime.proto file.
+var GTFSRTVERSION = "99fdfba627451d2861346c631af84535fa1e02fb"
