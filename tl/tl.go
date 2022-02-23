a9757e6a35fb09c440cfe4b7f7beaef6beb0aa06
Ian Rees <ian@ianrees.net>
2022-02-23 15:00:03 -0800

    Bump version to 0.9.0

--- tl/tl.go
+++ tl/tl.go
@@ -1,10 +1,10 @@
 package tl
 
 // VERSION is the current software version.
-var VERSION = "v0.8.8"
+var VERSION = "v0.9.0"
 
 // GTFSVERSION is the commit for the spec reference.md file.
-var GTFSVERSION = "fec428e9023a8dabb1a23cbd56ba607fe5e3e96c"
+var GTFSVERSION = "2e6887ea16b689d8ebc70ad334ac8abb2f94a66e"
 
 // GTFSRTVERSION is the commit for the gtfs-realtime.proto file.
 var GTFSRTVERSION = "99fdfba627451d2861346c631af84535fa1e02fb"
