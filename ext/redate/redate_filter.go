56c25cca4ae01e194270cb11a858f41f981dbf4d
Ian Rees <ian@ianrees.net>
2022-03-15 19:29:33 -0700

    Redate filter improvements and tests

--- ext/redate/redate_filter.go
+++ ext/redate/redate_filter.go
@@ -15,14 +15,16 @@ func init() {
 
 type RedateFilter struct {
 	StartDate           time.Time
+	StartDays           int
 	TargetDate          time.Time
 	TargetDays          int
 	RemoveOutsideWindow bool
 }
 
-func NewRedateFilter(startDate, targetDate time.Time, targetDays int) *RedateFilter {
+func NewRedateFilter(startDate, targetDate time.Time, startDays, targetDays int) *RedateFilter {
 	return &RedateFilter{
 		StartDate:  startDate,
+		StartDays:  startDays,
 		TargetDate: targetDate,
 		TargetDays: targetDays,
 	}
@@ -34,19 +36,30 @@ func (tf *RedateFilter) Filter(ent tl.Entity, emap *tl.EntityMap) error {
 		return nil
 	}
 	// Copy active service days in window into new calendar
+	active := false
 	startDate := tf.StartDate
 	targetDate := tf.TargetDate
-	targetDays := tf.TargetDays
 	newSvc := tl.NewService(tl.Calendar{ServiceID: v.ServiceID, StartDate: targetDate})
 	newSvc.ID = v.ID
-	active := false
-	for i := 0; i < targetDays-1; i++ {
+	fmt.Printf("%#v\n", v.Calendar)
+	for i := 1; i < tf.TargetDays; i++ {
+		a := false
 		if v.IsActive(startDate) {
 			newSvc.AddCalendarDate(tl.CalendarDate{Date: targetDate, ExceptionType: 1})
 			active = true
+			a = true
 		}
-		startDate = startDate.AddDate(0, 0, 1)
-		targetDate = targetDate.AddDate(0, 0, 1)
+		fmt.Println(
+			"svcId:", newSvc.ServiceID,
+			"startDate:", startDate,
+			startDate.Weekday().String(),
+			"targetDate:", targetDate,
+			targetDate.Weekday().String(),
+			"i:", i,
+			"a:", a,
+		)
+		startDate = tf.StartDate.AddDate(0, 0, i%tf.StartDays) // startDate.AddDate(0, 0, 1)
+		targetDate = tf.TargetDate.AddDate(0, 0, i)
 	}
 	newSvc.EndDate = targetDate
 	if !active && tf.RemoveOutsideWindow {
---
+++ ext/redate/redate_filter_test.go
@@ -0,0 +1,94 @@
+package redate
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/interline-io/transitland-lib/copier"
+	"github.com/interline-io/transitland-lib/internal/mock"
+	"github.com/interline-io/transitland-lib/internal/testutil"
+	"github.com/interline-io/transitland-lib/tl"
+	"github.com/interline-io/transitland-lib/tlcsv"
+	"github.com/stretchr/testify/assert"
+)
+
+var tft = "2006-01-02"
+
+func ptime(v string) time.Time {
+	t, _ := time.Parse(tft, v)
+	return t
+}
+
+func TestRedateFilter(t *testing.T) {
+	tcs := []struct {
+		Name      string
+		StartDate string
+		EndDate   string
+		StartDays int
+		EndDays   int
+		ExpectSvc tl.Service
+	}{
+		{"7/7 days", "2018-06-03", "2022-01-02", 7, 7, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-02"), EndDate: ptime("2022-01-08"), Monday: 1, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+		{"7/28 days", "2018-06-03", "2022-01-02", 7, 28, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-02"), EndDate: ptime("2022-01-29"), Monday: 1, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+		{"7/28 days sunday", "2018-06-03", "2022-01-02", 7, 28, tl.Service{Calendar: tl.Calendar{ServiceID: "SUN", StartDate: ptime("2022-01-02"), EndDate: ptime("2022-01-29"), Sunday: 1}}},
+		{"7/28 days monday holiday", "2018-05-27", "2022-01-02", 7, 28, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-02"), EndDate: ptime("2022-01-29"), Monday: 0, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+		{"7/365 days", "2018-06-03", "2022-01-02", 7, 365, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-02"), EndDate: ptime("2023-01-01"), Monday: 1, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+		{"7/1 days", "2018-06-03", "2022-01-02", 7, 1, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-02"), EndDate: ptime("2022-01-02"), Monday: 1, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+		{"1/1 days", "2018-06-03", "2022-01-02", 1, 1, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-02"), EndDate: ptime("2022-01-02"), Monday: 1, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+		{"1/7 days monday start", "2018-06-04", "2022-01-03", 1, 7, tl.Service{Calendar: tl.Calendar{ServiceID: "WKDY", StartDate: ptime("2022-01-03"), EndDate: ptime("2022-01-09"), Sunday: 1, Saturday: 1, Monday: 1, Tuesday: 1, Wednesday: 1, Thursday: 1, Friday: 1}}},
+	}
+	reader, err := tlcsv.NewReader(testutil.ExampleFeedBART.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, tc := range tcs {
+		name := fmt.Sprintf("%s-%s:%d:%d", tc.StartDate, tc.EndDate, tc.StartDays, tc.EndDays)
+		t.Run(name, func(t *testing.T) {
+			rf := RedateFilter{}
+			var err error
+			rf.StartDate, err = time.Parse(tft, tc.StartDate)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rf.TargetDate, err = time.Parse(tft, tc.EndDate)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rf.StartDays = tc.StartDays
+			rf.TargetDays = tc.EndDays
+			w := mock.Writer{}
+			cp, err := copier.NewCopier(reader, &w, copier.Options{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			cp.AddExtension(&rf)
+			cp.Copy()
+			wr, err := w.NewReader()
+			if err != nil {
+				t.Fatal(err)
+			}
+			svcs := tl.NewServicesFromReader(wr)
+			found := false
+			v := tc.ExpectSvc
+			for _, svc := range svcs {
+				if v.ServiceID != svc.ServiceID {
+					continue
+				}
+				found = true
+				assert.Equal(t, v.StartDate.Format(tft), svc.StartDate.Format(tft))
+				assert.Equal(t, v.EndDate.Format(tft), svc.EndDate.Format(tft))
+				startDate := v.StartDate
+				for startDate.Before(v.EndDate) {
+					a := v.IsActive(startDate)
+					b := svc.IsActive(startDate)
+					assert.Equalf(t, a, b, "expected active %t got %t on date %s", a, b, startDate.Format(tft))
+					startDate = startDate.AddDate(0, 0, 1)
+				}
+			}
+			if !found {
+				t.Errorf("did not match expected output service %s", v.ServiceID)
+			}
+		})
+	}
+}
