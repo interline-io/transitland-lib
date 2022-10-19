d37da3cc653a470f37340d4a5406c46213095423
Ian Rees <ian@ianrees.net>
2022-10-18 18:45:18 -0700

    Tidy

--- rules/duplicate_fare_transfer_rule.go
+++ rules/duplicate_fare_transfer_rule.go
@@ -19,10 +19,11 @@ type DuplicateFareTransferRuleError struct {
 
 func (e *DuplicateFareTransferRuleError) Error() string {
 	return fmt.Sprintf(
-		"fare_transfer_rule with fare_product_id '%s' from_leg_group_id '%s' to_leg_group_id '%s' transfer_count %d and duration_limit %d is duplicated",
+		"fare_transfer_rule with fare_product_id '%s' from_leg_group_id '%s' to_leg_group_id '%s' filter_fare_product_id '%s' transfer_count %d and duration_limit %d is duplicated",
 		e.FareProductID,
 		e.FromLegGroupID,
 		e.ToLegGroupID,
+		e.FilterFareProductID,
 		e.TransferCount,
 		e.DurationLimit,
 	)
@@ -45,9 +46,9 @@ func (e *DuplicateFareTransferRuleCheck) Validate(ent tl.Entity) []error {
 		FareProductID:       v.FareProductID.Val,
 		FromLegGroupID:      v.FromLegGroupID.Val,
 		ToLegGroupID:        v.ToLegGroupID.Val,
+		FilterFareProductID: v.FilterFareProductID.Val,
 		TransferCount:       v.TransferCount.Val,
 		DurationLimit:       v.DurationLimit.Val,
-		FilterFareProductID: v.FilterFareProductID.Val,
 	}
 	key := fmt.Sprintf("%s:%s:%s:%s:%d:%d", err.FareProductID, err.FromLegGroupID, err.ToLegGroupID, err.FilterFareProductID, err.TransferCount, err.DurationLimit)
 	if _, ok := e.vals[key]; ok {
