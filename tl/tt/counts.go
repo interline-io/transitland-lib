88caf2c66aa4297745bdb9036a406b1fbebd1ae0
Ian Rees <ian@ianrees.net>
2023-10-17 14:48:01 -0400

    Stub GQL methods

--- tl/tt/counts.go
+++ tl/tt/counts.go
@@ -4,19 +4,29 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
+	"io"
 )
 
 // Counts is a simple map[string]int with json support
 type Counts map[string]int
 
-func (a Counts) Value() (driver.Value, error) {
-	return json.Marshal(a)
+func (r Counts) Value() (driver.Value, error) {
+	return json.Marshal(r)
 }
 
-func (a *Counts) Scan(value interface{}) error {
+func (r *Counts) Scan(value interface{}) error {
 	b, ok := value.([]byte)
 	if !ok {
 		return errors.New("type assertion to []byte failed")
 	}
-	return json.Unmarshal(b, &a)
+	return json.Unmarshal(b, &r)
+}
+
+func (r *Counts) UnmarshalGQL(v interface{}) error {
+	return r.Scan(v)
+}
+
+func (r Counts) MarshalGQL(w io.Writer) {
+	// b, _ := r.MarshalJSON()
+	// w.Write(b)
 }
