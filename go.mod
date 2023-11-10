fdd485f088493e643e7d793cc9c25c1883c3b9c4
Ian Rees <ian@ianrees.net>
2023-11-10 13:02:50 -0800

    Start error testing

--- go.mod
+++ go.mod
@@ -43,6 +43,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.6 // indirect
 	github.com/aws/smithy-go v1.11.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set/v2 v2.3.1 // indirect
 	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
--- rt/validator_test.go
+++ rt/validator_test.go
@@ -3,8 +3,11 @@ package rt
 import (
 	"encoding/json"
 	"fmt"
+	"path/filepath"
 	"testing"
 	"time"
+
+	mapset "github.com/deckarep/golang-set/v2"
 
 	"github.com/interline-io/transitland-lib/adapters/empty"
 	"github.com/interline-io/transitland-lib/copier"
@@ -119,4 +122,76 @@ func TestVehiclePositionStats(t *testing.T) {
 	}
 	jj, _ := json.Marshal(stats)
 	fmt.Println(string(jj))
+}
+
+func TestValidatorErrors(t *testing.T) {
+	rp := func(p string) string {
+		return testutil.RelPath(filepath.Join("test/data/rt/", p))
+	}
+	rpe := func(p string) string {
+		return testutil.RelPath(filepath.Join("test/data/rt/errors", p))
+	}
+	ms := func(vals ...int) mapset.Set[int] {
+		a := mapset.NewSet[int]()
+		for _, v := range vals {
+			a.Add(v)
+		}
+		return a
+	}
+
+	tcs := []struct {
+		name          string
+		static        string
+		rt            string
+		expectError   mapset.Set[int]
+		expectWarning mapset.Set[int]
+	}{
+		{
+			name:        "ct-tu-invalid-stop",
+			static:      rp("ct.zip"),
+			rt:          rpe("ct-tu-invalid-stop.json"),
+			expectError: ms(11),
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			msg, err := ReadRTJson(tc.rt)
+			if err != nil {
+				t.Fatal(err)
+			}
+			r, err := tlcsv.NewReader(tc.static)
+			if err != nil {
+				t.Fatal(err)
+			}
+			cp, err := copier.NewCopier(r, &empty.Writer{}, copier.Options{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			ex := NewValidator()
+			cp.AddExtension(ex)
+			result := cp.Copy()
+			_ = result
+			rterrs := ex.ValidateFeedMessage(msg, nil)
+			foundErrs := mapset.NewSet[int]()
+			foundWarns := mapset.NewSet[int]()
+			for _, rterr := range rterrs {
+				if a, ok := rterr.(*RealtimeError); ok {
+					foundErrs.Add(a.Code)
+				}
+				if a, ok := rterr.(*RealtimeWarning); ok {
+					foundWarns.Add(a.Code)
+				}
+			}
+			if tc.expectError != nil {
+				if d := tc.expectError.SymmetricDifference(foundErrs); d.Cardinality() > 0 {
+					t.Errorf("expected errors %v, got %v", tc.expectError, foundErrs)
+				}
+			}
+			if tc.expectWarning != nil {
+				if d := tc.expectWarning.SymmetricDifference(foundErrs); d.Cardinality() > 0 {
+					t.Errorf("expected warnings %v, got %v", tc.expectWarning, foundWarns)
+				}
+			}
+		})
+	}
 }
