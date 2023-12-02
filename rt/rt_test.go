a1770669982827ce2a998c365ed08503d57eb3f1
Ian Rees <ian@ianrees.net>
2023-12-01 16:37:00 -0800

    Remove unused code

--- rt/rt_test.go
+++ rt/rt_test.go
@@ -1,15 +1,9 @@
 package rt
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/interline-io/transitland-lib/internal/testutil"
-	"github.com/interline-io/transitland-lib/rt/pb"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func Test_readmsg(t *testing.T) {
@@ -49,53 +43,5 @@ func TestReadFileError(t *testing.T) {
 	_, err := ReadFile(testutil.RelPath("test/data/example.zip"))
 	if err == nil {
 		t.Errorf("got no error, expected illegal tag")
-	}
-}
-
-func RTAsJson(msg *pb.FeedMessage) ([]byte, error) {
-	m := protojson.MarshalOptions{Indent: "  "}
-	jdata, err := m.Marshal(msg)
-	return jdata, err
-}
-
-func TestRTAsJson(t *testing.T) {
-	msg, err := ReadFile(testutil.RelPath("test/data/rt/ct-vehicle-positions.pb"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	jdata, err := RTAsJson(msg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(string(jdata))
-}
-
-// func Test_convert(t *testing.T) {
-// 	testConvert(t, testutil.RelPath("test/data/rt"))
-// }
-
-func testConvert(t *testing.T, dir string) {
-	fns, err := os.ReadDir(dir)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, fs := range fns {
-		fn := fs.Name()
-		newFn := filepath.Join(dir, fmt.Sprintf("%s.json", fn))
-		if !strings.HasSuffix(fn, ".pb") {
-			continue
-		}
-		msg, err := ReadFile(filepath.Join(dir, fn))
-		if err != nil {
-			t.Log(fn, err.Error())
-			continue
-		}
-		jdata, err := RTAsJson(msg)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if err := os.WriteFile(newFn, jdata, 0644); err != nil {
-			t.Fatal(err)
-		}
 	}
 }
