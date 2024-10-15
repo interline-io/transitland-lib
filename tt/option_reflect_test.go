06b736b1fb7497af4843305c4ddc39eab0261943
Ian Rees <ian@ianrees.net>
2024-10-14 17:20:52 -0700

    Defer testing for now

--- tt/option_reflect_test.go
+++ tt/option_reflect_test.go
@@ -1,31 +1,26 @@
 package tt
 
-import (
-	"fmt"
-	"testing"
-)
+// type ReflectCheckErrorsEntity struct {
+// 	PlainString         string
+// 	PlainStringRequired string `csv:",required"`
+// 	StopID              String `csv:",required"`
+// 	Name                String `csv:",required"`
+// 	Desc                String
+// 	Timezone            Timezone
+// 	LocationType        Int
+// 	AgencyURL           Url
+// 	BaseEntity
+// }
 
-type ReflectCheckErrorsEntity struct {
-	PlainString         string
-	PlainStringRequired string `csv:",required"`
-	StopID              String `csv:",required"`
-	Name                String `csv:",required"`
-	Desc                String
-	Timezone            Timezone
-	LocationType        Int
-	AgencyURL           Url
-	BaseEntity
-}
-
-func TestReflectCheckErrors(t *testing.T) {
-	ent := ReflectCheckErrorsEntity{
-		Name:         NewString("ok"),
-		LocationType: NewInt(2),
-		Timezone:     Timezone{Option: NewOption("asd")},
-		AgencyURL:    Url{Option: NewOption("xyz")},
-	}
-	entErrs := ReflectCheckErrors(&ent)
-	for _, entErr := range entErrs {
-		fmt.Printf("entErrs: %#v\n", entErr)
-	}
-}
+// func TestReflectCheckErrors(t *testing.T) {
+// 	ent := ReflectCheckErrorsEntity{
+// 		Name:         NewString("ok"),
+// 		LocationType: NewInt(2),
+// 		Timezone:     Timezone{Option: NewOption("asd")},
+// 		AgencyURL:    Url{Option: NewOption("xyz")},
+// 	}
+// 	entErrs := ReflectCheckErrors(&ent)
+// 	for _, entErr := range entErrs {
+// 		fmt.Printf("entErrs: %#v\n", entErr)
+// 	}
+// }
