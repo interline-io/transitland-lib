92e31d8ff1ca1143cf1fddf67f9b7e9beb2b2e54
Ian Rees <ian@ianrees.net>
2024-10-15 19:28:56 -0700

    Empty string postgres expression fixes

--- filters/notnull.go
+++ filters/notnull.go
@@ -14,9 +14,25 @@ type NotNullFilter struct{}
 func (e *NotNullFilter) Filter(ent tt.Entity, emap *tt.EntityMap) error {
 	switch v := ent.(type) {
 	case *gtfs.Agency:
+		// Used in textsearch
 		v.AgencyID.OrSet("")
+		v.AgencyURL.OrSet("")
+		v.AgencyEmail.OrSet("")
+		v.AgencyURL.OrSet("")
+	case *gtfs.Route:
+		// Used in textsearch
+		v.RouteLongName.OrSet("")
+		v.RouteShortName.OrSet("")
+		v.RouteDesc.OrSet("")
+		v.RouteID.OrSet("")
 	case *gtfs.Stop:
 		v.LocationType.OrSet(0)
+		// Used in textsearch
+		v.StopName.OrSet("")
+		v.StopDesc.OrSet("")
+		v.StopCode.OrSet("")
+		v.StopURL.OrSet("")
+		v.StopID.OrSet("")
 	case *gtfs.Trip:
 		v.DirectionID.OrSet(0)
 		v.StopPatternID.OrSet(0)
