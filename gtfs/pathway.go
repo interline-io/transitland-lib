5c4f3a25eb3b26e41c6a02c72600dfb4902f56aa
Ian Rees <ian@ianrees.net>
2024-10-19 00:30:55 -0700

    Remove pathway custom GetString

--- gtfs/pathway.go
+++ gtfs/pathway.go
@@ -1,9 +1,6 @@
 package gtfs
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/interline-io/transitland-lib/tt"
 )
 
@@ -45,45 +42,45 @@ func (ent *Pathway) TableName() string {
 }
 
 // GetString returns the string representation of an field.
-func (ent *Pathway) GetString(key string) (string, error) {
-	v := ""
-	switch key {
-	case "pathway_id":
-		v = ent.PathwayID.String()
-	case "from_stop_id":
-		v = ent.FromStopID.String()
-	case "to_stop_id":
-		v = ent.ToStopID.String()
-	case "pathway_mode":
-		v = ent.PathwayMode.String()
-	case "is_bidirectional":
-		v = ent.IsBidirectional.String()
-	case "length":
-		if ent.Length.Val > 0 {
-			v = fmt.Sprintf("%0.5f", ent.Length.Val)
-		}
-	case "traversal_time":
-		if ent.TraversalTime.Val > 0 {
-			v = ent.TraversalTime.String()
-		}
-	case "stair_count":
-		if ent.StairCount.Val != 0 && ent.StairCount.Val != -1 {
-			v = ent.StairCount.String()
-		}
-	case "max_slope":
-		if ent.MaxSlope.Val != 0 {
-			v = fmt.Sprintf("%0.2f", ent.MaxSlope.Val)
-		}
-	case "min_width":
-		if ent.MinWidth.Val != 0 {
-			v = fmt.Sprintf("%0.2f", ent.MinWidth.Val)
-		}
-	case "signposted_as":
-		v = ent.SignpostedAs.String()
-	case "reversed_signposted_as":
-		v = ent.ReverseSignpostedAs.String()
-	default:
-		return v, errors.New("unknown key")
-	}
-	return v, nil
-}
+// func (ent *Pathway) GetString(key string) (string, error) {
+// 	v := ""
+// 	switch key {
+// 	case "pathway_id":
+// 		v = ent.PathwayID.String()
+// 	case "from_stop_id":
+// 		v = ent.FromStopID.String()
+// 	case "to_stop_id":
+// 		v = ent.ToStopID.String()
+// 	case "pathway_mode":
+// 		v = ent.PathwayMode.String()
+// 	case "is_bidirectional":
+// 		v = ent.IsBidirectional.String()
+// 	case "length":
+// 		if ent.Length.Val > 0 {
+// 			v = fmt.Sprintf("%0.5f", ent.Length.Val)
+// 		}
+// 	case "traversal_time":
+// 		if ent.TraversalTime.Val > 0 {
+// 			v = ent.TraversalTime.String()
+// 		}
+// 	case "stair_count":
+// 		if ent.StairCount.Val != 0 && ent.StairCount.Val != -1 {
+// 			v = ent.StairCount.String()
+// 		}
+// 	case "max_slope":
+// 		if ent.MaxSlope.Val != 0 {
+// 			v = fmt.Sprintf("%0.2f", ent.MaxSlope.Val)
+// 		}
+// 	case "min_width":
+// 		if ent.MinWidth.Val != 0 {
+// 			v = fmt.Sprintf("%0.2f", ent.MinWidth.Val)
+// 		}
+// 	case "signposted_as":
+// 		v = ent.SignpostedAs.String()
+// 	case "reversed_signposted_as":
+// 		v = ent.ReverseSignpostedAs.String()
+// 	default:
+// 		return v, errors.New("unknown key")
+// 	}
+// 	return v, nil
+// }
