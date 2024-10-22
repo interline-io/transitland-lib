cfb4af410f0bf1bdac50528066dd01f0100814de
Ian Rees <ian@ianrees.net>
2024-10-21 18:29:47 -0700

    Add ReadRowsIter method

--- tlcsv/row.go
+++ tlcsv/row.go
@@ -3,6 +3,7 @@ package tlcsv
 import (
 	"encoding/csv"
 	"io"
+	"iter"
 	"strings"
 
 	"github.com/dimchansky/utfbom"
@@ -28,25 +29,6 @@ func (row *Row) Get(k string) (string, bool) {
 }
 
 type csvOptFn func(*csv.Reader)
-
-func ReadRowsOptions(in io.Reader, cb func(Row), optFns ...csvOptFn) error {
-	// Handle byte-order-marks.
-	r := csv.NewReader(utfbom.SkipOnly(in))
-	// Allow variable columns - very common in GTFS
-	r.FieldsPerRecord = -1
-	// Trimming is done elsewhere
-	r.TrimLeadingSpace = false
-	// Reuse record
-	r.ReuseRecord = true
-	// Allow unescaped quotes
-	r.LazyQuotes = true
-	// Add additional options
-	for _, optFn := range optFns {
-		optFn(r)
-	}
-	// Go
-	return readRows(r, cb)
-}
 
 // ReadRows iterates through csv rows with callback.
 func ReadRows(in io.Reader, cb func(Row)) error {
@@ -106,4 +88,68 @@ func readRows(r *csv.Reader, cb func(Row)) error {
 		cb(Row{Row: row, Line: line, Header: header, Hindex: hindex, Err: err})
 	}
 	return nil
+}
+
+func ReadRowsIter(in io.Reader, optFns ...csvOptFn) iter.Seq2[Row, error] {
+	// Handle byte-order-marks.
+	r := csv.NewReader(utfbom.SkipOnly(in))
+	// Allow variable columns - very common in GTFS
+	r.FieldsPerRecord = -1
+	// Trimming is done elsewhere
+	r.TrimLeadingSpace = false
+	// Reuse record
+	r.ReuseRecord = true
+	// Allow unescaped quotes
+	r.LazyQuotes = true
+	// Add additional options
+	for _, optFn := range optFns {
+		optFn(r)
+	}
+	return func(yield func(Row, error) bool) {
+		// Go for it.
+		firstRow, err := r.Read()
+		if err != nil {
+			yield(Row{}, err)
+			return
+		}
+		// Copy header, since we will reuse the backing array
+		header := []string{}
+		for _, v := range firstRow {
+			header = append(header, strings.TrimSpace(v))
+		}
+		// Map the header to row index
+		hindex := map[string]int{}
+		for k, i := range header {
+			hindex[i] = k
+		}
+		for {
+			row, err := r.Read()
+			if err == nil {
+				// ok
+			} else if err == io.EOF {
+				break
+			} else if _, ok := err.(*csv.ParseError); ok {
+				// Parse error: clear row, add error to row
+				row = []string{}
+			} else {
+				// Serious error: break and return with error
+				yield(Row{}, err)
+				return
+			}
+			// Remove whitespace
+			for i := 0; i < len(row); i++ {
+				v := row[i]
+				// This is dumb but saves substantial time.
+				if len(v) > 0 && (v[0] == ' ' || v[len(v)-1] == ' ' || v[0] == '\t' || v[len(v)-1] == '\t') {
+					row[i] = strings.TrimSpace(v)
+				}
+			}
+			// Pass parse errors to row
+			line, _ := r.FieldPos(0)
+			cbrow := Row{Row: row, Line: line, Header: header, Hindex: hindex, Err: err}
+			if !yield(cbrow, nil) {
+				return
+			}
+		}
+	}
 }
