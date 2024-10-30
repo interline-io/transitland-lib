571dbca728234f63b36234b60a685ade6dc1c7af
Ian Rees <ian@ianrees.net>
2024-10-30 12:20:16 -0400

    Fix expected error

--- gtfs/calendar_date.go
+++ gtfs/calendar_date.go
@@ -1,9 +1,6 @@
 package gtfs
 
 import (
-	"fmt"
-
-	"github.com/interline-io/transitland-lib/causes"
 	"github.com/interline-io/transitland-lib/tt"
 )
 
@@ -15,16 +12,6 @@ type CalendarDate struct {
 	tt.BaseEntity
 }
 
-// Errors for this Entity.
-func (ent *CalendarDate) Errors() (errs []error) {
-	errs = append(errs, tt.CheckPresent("service_id", ent.ServiceID.Val)...)
-	errs = append(errs, tt.CheckInsideRangeInt("exception_type", ent.ExceptionType.Val, 1, 2)...)
-	if ent.Date.IsZero() {
-		errs = append(errs, causes.NewInvalidFieldError("date", "", fmt.Errorf("date is zero")))
-	}
-	return errs
-}
-
 // Filename calendar_dates.txt
 func (ent *CalendarDate) Filename() string {
 	return "calendar_dates.txt"
@@ -33,14 +20,4 @@ func (ent *CalendarDate) Filename() string {
 // TableName gtfs_calendar_dates
 func (ent *CalendarDate) TableName() string {
 	return "gtfs_calendar_dates"
-}
-
-// UpdateKeys updates Entity references.
-func (ent *CalendarDate) UpdateKeys(emap *tt.EntityMap) error {
-	if serviceID, ok := emap.GetEntity(&Calendar{ServiceID: ent.ServiceID.Val}); ok {
-		ent.ServiceID.Set(serviceID)
-	} else {
-		return causes.NewInvalidReferenceError("service_id", ent.ServiceID.Val)
-	}
-	return nil
 }
