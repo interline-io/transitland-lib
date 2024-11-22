da2da837afd896f987858a356261f58f5a2525ce
Ian Rees <ian@ianrees.net>
2024-11-21 16:34:29 -0800

    Move

--- filters/route_network_id.go
+++ filters/route_network_id.go
@@ -1,22 +1,14 @@
-package rules
+package filters
 
 import (
-	"fmt"
-
 	"github.com/interline-io/transitland-lib/gtfs"
 	"github.com/interline-io/transitland-lib/tt"
 )
 
 type RouteNetworkIDFilter struct{}
 
-func (e *RouteNetworkIDFilter) Filter(ent tt.Entity, emap *tt.EntityMap) error {
-	if v, ok := ent.(*gtfs.Route); ok {
-		v.NetworkID = tt.String{}
-	}
-	return nil
-}
-
 func (e *RouteNetworkIDFilter) Expand(ent tt.Entity, emap *tt.EntityMap) ([]tt.Entity, bool, error) {
+	// Check if route and has NetworkID set
 	v, ok := ent.(*gtfs.Route)
 	if !ok {
 		return nil, false, nil
@@ -24,19 +16,25 @@ func (e *RouteNetworkIDFilter) Expand(ent tt.Entity, emap *tt.EntityMap) ([]tt.E
 	if !v.NetworkID.Valid {
 		return nil, false, nil
 	}
-	fmt.Println("RN:", v.NetworkID, v.RouteID)
+	// Expand into route + route_network + possible network
 	var ret []tt.Entity
 	ret = append(ret, ent)
 	if _, ok := emap.Get("networks.txt", v.NetworkID.Val); !ok {
 		n := gtfs.Network{}
 		n.NetworkID.Set(v.NetworkID.Val)
 		ret = append(ret, &n)
-		fmt.Println("CREATING NETWORK:", n)
 	}
 	rn := gtfs.RouteNetwork{}
 	rn.NetworkID.Set(v.NetworkID.Val)
 	rn.RouteID.Set(v.RouteID.Val)
 	ret = append(ret, &rn)
-	fmt.Println("CREATING ROUTE NETWORK:", rn)
 	return ret, true, nil
+}
+
+func (e *RouteNetworkIDFilter) Filter(ent tt.Entity, emap *tt.EntityMap) error {
+	// Unset any set NetworkID
+	if v, ok := ent.(*gtfs.Route); ok {
+		v.NetworkID = tt.String{}
+	}
+	return nil
 }
