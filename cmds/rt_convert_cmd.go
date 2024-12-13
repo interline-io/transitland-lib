cafa596f49cd7a4d5078b34f1e3e992a6254ad58
Ian Rees <ian@ianrees.net>
2024-12-13 13:26:40 -0800

    Simplify command

--- cmds/rt_convert_cmd.go
+++ cmds/rt_convert_cmd.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"os"
 
-	"github.com/interline-io/log"
 	"github.com/interline-io/transitland-lib/request"
 	"github.com/interline-io/transitland-lib/rt"
 	"github.com/interline-io/transitland-lib/tlcli"
@@ -23,14 +22,15 @@ func (cmd *RTConvertCommand) HelpDesc() (string, string) {
 }
 
 func (cmd *RTConvertCommand) HelpExample() string {
-	return `% {{.ParentCommand}} {{.Command}} "trips.pb" "trips.pb.json"`
+	return `% {{.ParentCommand}} {{.Command}} "trips.pb"`
 }
 
 func (cmd *RTConvertCommand) HelpArgs() string {
-	return "[flags] <input pb> <output json>"
+	return "[flags] <input pb>"
 }
 
 func (cmd *RTConvertCommand) AddFlags(fl *pflag.FlagSet) {
+	fl.StringVarP(&cmd.OutputFile, "out", "o", "", "Write JSON to file; defaults to stdout")
 }
 
 func (cmd *RTConvertCommand) Parse(args []string) error {
@@ -38,30 +38,34 @@ func (cmd *RTConvertCommand) Parse(args []string) error {
 	if fl.NArg() < 1 {
 		return errors.New("requires input pb")
 	}
-	if fl.NArg() < 2 {
-		return errors.New("requires output json file")
-	}
 	cmd.InputFile = fl.Arg(0)
-	cmd.OutputFile = fl.Arg(1)
 	return nil
 }
 
 func (cmd *RTConvertCommand) Run() error {
-	log.Info().Msgf("Converting '%s' to '%s'", cmd.InputFile, cmd.OutputFile)
+	// Fetch
 	msg, err := rt.ReadURL(cmd.InputFile, request.WithAllowLocal)
 	if err != nil {
 		return err
 	}
-	mOpts := protojson.MarshalOptions{UseProtoNames: true}
+	// Create json
+	mOpts := protojson.MarshalOptions{UseProtoNames: true, Indent: "  "}
 	rtJson, err := mOpts.Marshal(msg)
 	if err != nil {
 		return err
 	}
-	outf, err := os.Create(cmd.OutputFile)
-	if err != nil {
+	// Write
+	outf := os.Stdout
+	if cmd.OutputFile != "" {
+		var err error
+		outf, err = os.Create(cmd.OutputFile)
+		if err != nil {
+			return err
+		}
+		defer outf.Close()
+	}
+	if _, err := outf.Write(rtJson); err != nil {
 		return err
 	}
-	defer outf.Close()
-	outf.Write(rtJson)
 	return nil
 }
