ebcffc9a57a8dda58ed425a2a03f8f0bb81cb6df
Ian Rees <ian@ianrees.net>
2025-05-22 16:00:29 -0700

    Load NE data

--- cmds/dbmigrate_cmd.go
+++ cmds/dbmigrate_cmd.go
@@ -6,11 +6,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"os"
-	"path/filepath"
-	"reflect"
 	"strings"
 
 	"github.com/golang-migrate/migrate/v4"
@@ -18,13 +15,15 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 	_ "github.com/golang-migrate/migrate/v4/source/iofs"
-	"github.com/jmoiron/sqlx"
-	"github.com/jonas-p/go-shp"
+	"github.com/rs/zerolog"
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-shapefile"
 
 	"github.com/interline-io/log"
 	"github.com/interline-io/transitland-lib/schema/ne"
 	postgressMigrations "github.com/interline-io/transitland-lib/schema/postgres"
 	"github.com/interline-io/transitland-lib/tlcli"
+	"github.com/interline-io/transitland-lib/tldb"
 	postgressAdapter "github.com/interline-io/transitland-lib/tldb/postgres"
 	"github.com/interline-io/transitland-lib/tt"
 
@@ -34,6 +33,7 @@ import (
 type DBMigrateCommand struct {
 	DBURL      string
 	Subcommand string
+	Adapter    tldb.Adapter
 }
 
 func (cmd *DBMigrateCommand) HelpDesc() (string, string) {
@@ -68,6 +68,9 @@ func (cmd *DBMigrateCommand) Run(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+	defer atx.Close()
+	cmd.Adapter = postgressAdapter.NewPostgresAdapterFromDBX(db)
+
 	_ = postgressMigrations.EmbeddedMigrations
 	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
 	if err != nil {
@@ -79,22 +82,32 @@ func (cmd *DBMigrateCommand) Run(ctx context.Context) error {
 		return err
 	}
 	m, err := migrate.NewWithInstance("iofs", source, "postgres", driver)
+	m.Log = &migrationLogger{log: log.Logger.With().Logger()}
 	if err != nil {
 		return err
 	}
 	switch cmd.Subcommand {
 	case "up":
-		return m.Up()
+		log.Info().Msg("Running migrations...")
+		err := m.Up()
+		if err == nil {
+			log.Info().Msg("Migrations complete")
+			return nil
+		}
+		if err == migrate.ErrNoChange {
+			log.Info().Msg("No migrations to run")
+			return nil
+		}
+		return err
 	case "natural-earth":
-		return cmd.neLoad(ctx, db)
+		return cmd.neLoad(ctx)
 	case "down":
 		return errors.New("unsupported command")
 	}
 	return fmt.Errorf("unknown subcommand: %s", cmd.Subcommand)
 }
 
-func (cmd *DBMigrateCommand) neLoad(ctx context.Context, db *sqlx.DB) error {
-	// Teporarily write out to zip
+func (cmd *DBMigrateCommand) neLoad(ctx context.Context) error {
 	var err error
 	err = fs.WalkDir(ne.EmbeddedNaturalEarthData, ".", func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
@@ -110,69 +123,71 @@ func (cmd *DBMigrateCommand) neLoad(ctx context.Context, db *sqlx.DB) error {
 		}
 		switch path {
 		case "ne_10m_admin_1_states_provinces.zip":
-			return cmd.neProcess(ctx, db, neZipData, cmd.neLoadAdmins)
+			return cmd.neProcess(ctx, neZipData, cmd.neLoadAdmins)
 		case "ne_10m_populated_places_simple.zip":
+			return cmd.neProcess(ctx, neZipData, cmd.neLoadPlaces)
 		}
 		return nil
 	})
 	return err
 }
 
-type shpFileHandler = func(context.Context, *sqlx.DB, *shp.Reader) error
-
-func (cmd *DBMigrateCommand) neProcess(ctx context.Context, db *sqlx.DB, neZipFile []byte, cb shpFileHandler) error {
-	neTempDir, err := os.MkdirTemp("", "ne-shape")
+func (cmd *DBMigrateCommand) neProcess(ctx context.Context, neZipFile []byte, cb shpFileHandler) error {
+	ret := []neShape{}
+	zipReader, err := zip.NewReader(bytes.NewReader(neZipFile), int64(len(neZipFile)))
+	shape, err := shapefile.NewScannerFromZipReader(zipReader, &shapefile.ReadShapefileOptions{})
 	if err != nil {
 		return err
 	}
-	defer os.RemoveAll(neTempDir)
-	buf := bytes.NewReader(neZipFile)
-	zipReader, err := zip.NewReader(buf, buf.Size())
-	shpFileName := ""
-	for _, zipFile := range zipReader.File {
-		log.Infof("copying file %s", zipFile.Name)
-		neOutTmp, err := os.Create(filepath.Join(neTempDir, zipFile.Name))
-		if err != nil {
-			return err
+	shapeFields := shape.DBFFieldDescriptors()
+	for shape.Next() {
+		shpGeom, _, shpRec := shape.Scan()
+		if shpGeom == nil {
+			continue
 		}
-		zipFileReader, err := zipFile.Open()
-		if _, err := io.Copy(neOutTmp, zipFileReader); err != nil {
-			return err
+		t := neShape{}
+		t.attrs = map[string]string{}
+		for i, fieldDesc := range shapeFields {
+			fieldName := fieldDesc.Name
+			fieldValue := shpRec[i]
+			t.attrs[fieldName] = fmt.Sprintf("%v", fieldValue)
 		}
-		if err := neOutTmp.Close(); err != nil {
-			return err
-		}
-		if strings.HasSuffix(neOutTmp.Name(), ".shp") {
-			shpFileName = neOutTmp.Name()
-		}
+		t.geometry = shpGeom.Geom
+		ret = append(ret, t)
 	}
-	shpSrc, err := shp.Open(shpFileName)
-	if err != nil {
-		return err
-	}
-	return cb(ctx, db, shpSrc)
+	return cb(ctx, cmd.Adapter, ret)
 }
+
+type shpFileHandler = func(context.Context, tldb.Adapter, []neShape) error
+
+type neShape struct {
+	geometry geom.T
+	attrs    map[string]string
+}
+
+//////////////////
 
 type neAdmin struct {
 	Name     tt.String   `db:"name"`
+	Admin    tt.String   `db:"admin"`
 	IsoName  tt.String   `db:"iso_3166_2"`
 	IsoA2    tt.String   `db:"iso_a2"`
 	Geometry tt.Geometry `db:"geometry"`
 }
 
-func (cmd *DBMigrateCommand) neLoadAdmins(ctx context.Context, db *sqlx.DB, shpSrc *shp.Reader) error {
-	shpFields := shpSrc.Fields()
-	defer shpSrc.Close()
-	for shpSrc.Next() {
-		n, p := shpSrc.Shape()
+func (ent *neAdmin) TableName() string {
+	return "ne_10m_admin_1_states_provinces"
+}
 
-		fmt.Println(reflect.TypeOf(p).Elem(), p.BBox())
-
-		// print attributes
+func (cmd *DBMigrateCommand) neLoadAdmins(ctx context.Context, atx tldb.Adapter, shapes []neShape) error {
+	var ents []any
+	for _, nes := range shapes {
 		ent := neAdmin{}
-		for k, f := range shpFields {
-			val := shpSrc.ReadAttribute(n, k)
-			switch f.String() {
+		ent.Geometry = tt.NewGeometry(nes.geometry)
+		for k, val := range nes.attrs {
+			switch k {
+			case "admin":
+				ent.Admin.Set(val)
 			case "name":
 				ent.Name.Set(val)
 			case "iso_3166_2":
@@ -181,7 +196,64 @@ func (cmd *DBMigrateCommand) neLoadAdmins(ctx context.Context, db *sqlx.DB, shpS
 				ent.IsoA2.Set(val)
 			}
 		}
-		fmt.Println("ent:", ent)
+		ents = append(ents, &ent)
+	}
+	if _, err := cmd.Adapter.MultiInsert(ctx, ents); err != nil {
+		return err
 	}
 	return nil
+}
+
+//////////////////
+
+type nePlace struct {
+	Name     tt.String   `db:"name"`
+	Adm0Name tt.String   `db:"adm0name"`
+	Adm1Name tt.String   `db:"adm1name"`
+	IsoA2    tt.String   `db:"iso_a2"`
+	Geometry tt.Geometry `db:"geometry"`
+}
+
+func (ent *nePlace) TableName() string {
+	return "ne_10m_populated_places"
+}
+
+func (cmd *DBMigrateCommand) neLoadPlaces(ctx context.Context, atx tldb.Adapter, shapes []neShape) error {
+	var ents []any
+	for _, nes := range shapes {
+		ent := nePlace{}
+		ent.Geometry = tt.NewGeometry(nes.geometry)
+		for k, val := range nes.attrs {
+			switch k {
+			case "name":
+				ent.Name.Set(val)
+			case "adm0name":
+				ent.Adm0Name.Set(val)
+			case "adm1name":
+				ent.Adm1Name.Set(val)
+			case "iso_a2":
+				ent.IsoA2.Set(val)
+			}
+		}
+		ents = append(ents, &ent)
+	}
+	if _, err := cmd.Adapter.MultiInsert(ctx, ents); err != nil {
+		return err
+	}
+	return nil
+}
+
+/////////
+
+type migrationLogger struct {
+	log zerolog.Logger
+}
+
+func (l *migrationLogger) Printf(format string, v ...interface{}) {
+	format = strings.TrimSuffix(format, "\n")
+	l.log.Info().Msgf(format, v...)
+}
+
+func (l *migrationLogger) Verbose() bool {
+	return false
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.24.2
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.11.1
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.6.0
-	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.5.1
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.0.0
 	github.com/Masterminds/squirrel v1.5.3
 	github.com/aws/aws-sdk-go-v2 v1.36.3
 	github.com/aws/aws-sdk-go-v2/config v1.29.14
@@ -13,11 +13,13 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.79.3
 	github.com/deckarep/golang-set/v2 v2.6.0
 	github.com/dimchansky/utfbom v1.1.1
+	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/iancoleman/orderedmap v0.2.0
 	github.com/interline-io/log v0.0.0-20250425230611-851ec713ec98
 	github.com/jackc/pgx/v5 v5.7.0
 	github.com/jlaffaye/ftp v0.0.0-20220524001917-dfa1e758f3af
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/jonas-p/go-shp v0.1.1
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/mmcloughlin/geohash v0.10.0
 	github.com/pkg/errors v0.9.1
@@ -64,12 +66,14 @@ require (
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/tidwall/geoindex v1.7.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
