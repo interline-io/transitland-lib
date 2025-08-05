3b9edc88690d09a6bad251ba32c1bb4279b34ca2
Drew Dara-Abrams <drew@interline.io>
2025-08-05 14:16:09 -0700

    Convert GTFS Realtime Vehicle Positions to GeoJSON or GeoJSONL (#467)
    * Convert GTFS Realtime Vehicle Positions to GeoJSON or GeoJSONL
    
    * doc update
    
    * when no VPs, just emit empty geojson/geojsonl
    
    * regenerate docs
    
    * one more doc page updated

--- cmds/rt_convert_cmd.go
+++ cmds/rt_convert_cmd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"os"
+	"strings"
 
 	"github.com/interline-io/transitland-lib/request"
 	"github.com/interline-io/transitland-lib/rt"
@@ -16,14 +17,21 @@ import (
 type RTConvertCommand struct {
 	InputFile  string
 	OutputFile string
+	Format     string
 }
 
 func (cmd *RTConvertCommand) HelpDesc() (string, string) {
-	return "Convert GTFS-RealTime to JSON", ""
+	return "Convert GTFS Realtime to JSON.", "Convert GTFS Realtime protocol buffer files to JSON format. Eases inspecting live feeds. Enables processing with JSON-based tools like jq. For vehicle position feeds, you can also convert to GeoJSON (FeatureCollection) or GeoJSONL (one feature per line) formats for visualization or geographic analysis. See https://www.interline.io/blog/geojsonl-extracts/ for more information about GeoJSONL. Note: GeoJSON formats only include vehicle position; trip updates and service alerts can be converted to JSON but not GeoJSON/GeoJSONL."
 }
 
 func (cmd *RTConvertCommand) HelpExample() string {
-	return `% {{.ParentCommand}} {{.Command}} "trips.pb"`
+	return `% {{.ParentCommand}} {{.Command}} "trips.pb"
+% {{.ParentCommand}} {{.Command}} --format geojson "vehicle_positions.pb"
+% {{.ParentCommand}} {{.Command}} --format geojsonl "vehicle_positions.pb"
+% {{.ParentCommand}} {{.Command}} --format json --out output.json "alerts.pb"
+% {{.ParentCommand}} {{.Command}} --format geojson --out mbta_vehicles.geojson "https://cdn.mbta.com/realtime/VehiclePositions.pb"
+% {{.ParentCommand}} {{.Command}} --format geojson "https://developer.trimet.org/ws/gtfs/VehiclePositions"
+% {{.ParentCommand}} {{.Command}} --format json "https://developer.trimet.org/ws/V1/TripUpdate"`
 }
 
 func (cmd *RTConvertCommand) HelpArgs() string {
@@ -31,7 +39,8 @@ func (cmd *RTConvertCommand) HelpArgs() string {
 }
 
 func (cmd *RTConvertCommand) AddFlags(fl *pflag.FlagSet) {
-	fl.StringVarP(&cmd.OutputFile, "out", "o", "", "Write JSON to file; defaults to stdout")
+	fl.StringVarP(&cmd.OutputFile, "out", "o", "", "Write output to file; defaults to stdout")
+	fl.StringVarP(&cmd.Format, "format", "f", "json", "Output format: json, geojson, geojsonl (geojson formats only convert vehicle position entities)")
 }
 
 func (cmd *RTConvertCommand) Parse(args []string) error {
@@ -49,12 +58,28 @@ func (cmd *RTConvertCommand) Run(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	// Create json
-	mOpts := protojson.MarshalOptions{UseProtoNames: true, Indent: "  "}
-	rtJson, err := mOpts.Marshal(msg)
-	if err != nil {
-		return err
+
+	var outputData []byte
+
+	// Handle different output formats
+	switch strings.ToLower(cmd.Format) {
+	case "json":
+		// Create json
+		mOpts := protojson.MarshalOptions{UseProtoNames: true, Indent: "  "}
+		outputData, err = mOpts.Marshal(msg)
+		if err != nil {
+			return err
+		}
+	case "geojson", "geojsonl":
+		// Convert to GeoJSON - will handle empty vehicle positions gracefully
+		outputData, err = rt.VehiclePositionsToGeoJSON(msg, cmd.Format == "geojsonl")
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New("unsupported format: " + cmd.Format)
 	}
+
 	// Write
 	outf := os.Stdout
 	if cmd.OutputFile != "" {
@@ -65,7 +90,7 @@ func (cmd *RTConvertCommand) Run(ctx context.Context) error {
 		}
 		defer outf.Close()
 	}
-	if _, err := outf.Write(rtJson); err != nil {
+	if _, err := outf.Write(outputData); err != nil {
 		return err
 	}
 	return nil
