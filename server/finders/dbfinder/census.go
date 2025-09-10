f4d76b47380aee358db0d37f1fc9bc501998662a
Ian Rees <ian@ianrees.net>
2025-09-10 00:34:58 -0700

    Fix MatchEntityID

--- server/finders/dbfinder/census.go
+++ server/finders/dbfinder/census.go
@@ -370,27 +370,30 @@ func censusDatasetGeographySelect(limit *int, where *model.CensusDatasetGeograph
 		Limit(checkLimit(limit))
 
 	if where != nil && where.Location != nil {
+		// qJoin must have a buffer and match_entity_id column
 		loc := where.Location
 		found := true
 		var qJoin sq.SelectBuilder
 		if loc.Bbox != nil {
-			qJoin = sq.StatementBuilder.Select().Column("ST_MakeEnvelope(?,?,?,?,4326) as buffer", loc.Bbox.MinLon, loc.Bbox.MinLat, loc.Bbox.MaxLon, loc.Bbox.MaxLat)
+			qJoin = sq.StatementBuilder.Select().Column("ST_MakeEnvelope(?,?,?,?,4326) as buffer", loc.Bbox.MinLon, loc.Bbox.MinLat, loc.Bbox.MaxLon, loc.Bbox.MaxLat).Column("0 as match_entity_id")
 		} else if loc.Within != nil && loc.Within.Valid {
 			jj, _ := geojson.Marshal(loc.Within.Val)
-			qJoin = sq.StatementBuilder.Select().Column("ST_GeomFromGeoJSON(?) as buffer", string(jj))
+			qJoin = sq.StatementBuilder.Select().Column("ST_GeomFromGeoJSON(?) as buffer", string(jj)).Column("0 as match_entity_id")
 		} else if loc.Near != nil {
 			radius := checkFloat(&loc.Near.Radius, 0, 1_000_000)
-			qJoin = sq.StatementBuilder.Select().Column("ST_Buffer(ST_MakePoint(?,?)::geography, ?) as buffer", loc.Near.Lon, loc.Near.Lat, radius)
+			qJoin = sq.StatementBuilder.Select().Column("ST_Buffer(ST_MakePoint(?,?)::geography, ?) as buffer", loc.Near.Lon, loc.Near.Lat, radius).Column("0 as match_entity_id")
 		} else if loc.StopBuffer != nil && len(loc.StopBuffer.StopIds) > 0 {
 			radius := checkFloat(loc.StopBuffer.Radius, 0, 1_000)
 			if radius == 0 {
 				qJoin = sq.StatementBuilder.Select().
 					Column("gtfs_stops.geometry as buffer").
+					Column("gtfs_stops.id as match_entity_id").
 					From("gtfs_stops").
 					Where(In("gtfs_stops.id", loc.StopBuffer.StopIds))
 
 			} else {
 				qJoin = sq.StatementBuilder.Select().
+					Column("0 as match_entity_id").
 					Column("ST_Buffer(ST_Collect(ST_Buffer(gtfs_stops.geometry::geography, ?)::geometry), 0) as buffer", radius).
 					From("gtfs_stops").
 					Where(In("gtfs_stops.id", loc.StopBuffer.StopIds))
@@ -400,7 +403,7 @@ func censusDatasetGeographySelect(limit *int, where *model.CensusDatasetGeograph
 		}
 		if found {
 			q = q.JoinClause(qJoin.Prefix("join (").Suffix(") as buffer on true"))
-			q = q.Where("ST_Intersects(tlcg.geometry, buffer.buffer)")
+			q = q.Where("ST_Intersects(tlcg.geometry, buffer.buffer)").Column("buffer.match_entity_id as match_entity_id")
 			if fields.intersectionArea {
 				q = q.Column("ST_Area(ST_Intersection(tlcg.geometry, buffer.buffer)) as intersection_area")
 			}
