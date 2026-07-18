interline-io/transitland-lib

commit 8ff62cca517a20927c808fb0e58f6a6cd1ecc8e0

Focus points (#520)
* Add schema support for "focus" for routes, stops, agencies

server/finders/dbfinder/route.go
@@ -10,14 +10,14 @@ import (
 
 func (f *Finder) FindRoutes(ctx context.Context, limit *int, after *model.Cursor, ids []int, where *model.RouteFilter) ([]*model.Route, error) {
 	var ents []*model.Route
-	useActive := UseActive{
-		Active:          true,
-		UseMaterialized: model.ForContext(ctx).UseMaterialized,
+	useActive := &UseActive{
+		active:       true,
+		materialized: model.ForContext(ctx).UseMaterialized,
 	}
 	if len(ids) > 0 || (where != nil && where.FeedVersionSha1 != nil) {
-		useActive.Active = false
+		useActive.active = false
 	}
-	q := routeSelect(limit, after, ids, &useActive, f.PermFilter(ctx), where)
+	q := routeSelect(limit, after, ids, useActive, f.PermFilter(ctx), where)
 	if err := dbutil.Select(ctx, f.db, q, &ents); err != nil {
 		return nil, logErr(ctx, err)
 	}
@@ -183,10 +183,6 @@ func (f *Finder) ShapesByIDs(ctx context.Context, ids []int) ([]*model.Shape, []
 }
 
 func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActive, permFilter *model.PermFilter, where *model.RouteFilter) sq.SelectBuilder {
-	routeTable := "gtfs_routes"
-	if useActive != nil && useActive.UseMaterialized {
-		routeTable = "tl_materialized_active_routes as gtfs_routes"
-	}
 	q := sq.StatementBuilder.Select(
 		"gtfs_routes.id",
 		"gtfs_routes.feed_version_id",
@@ -207,12 +203,14 @@ func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActiv
 		"current_feeds.id AS feed_id",
 		"current_feeds.onestop_id AS feed_onestop_id",
 		"feed_versions.sha1 AS feed_version_sha1",
-		"coalesce(feed_version_route_onestop_ids.onestop_id, '') as onestop_id",
+		useActive.UseTable(
+			"coalesce(feed_version_route_onestop_ids.onestop_id, '') as onestop_id",
+			"coalesce(gtfs_routes.onestop_id, '') as onestop_id",
+		),
 	).
-		From(routeTable).
+		From(useActive.UseTable("gtfs_routes", "tl_materialized_active_routes as gtfs_routes")).
 		Join("feed_versions ON feed_versions.id = gtfs_routes.feed_version_id").
 		Join("current_feeds ON current_feeds.id = feed_versions.feed_id").
-		OrderBy("gtfs_routes.feed_version_id,gtfs_routes.id").
 		Limit(finderCheckLimit(limit))
 
 	// Handle previous OnestopIds
@@ -222,7 +220,11 @@ func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActiv
 		}
 		if len(where.OnestopIds) > 0 && where.AllowPreviousOnestopIds != nil && *where.AllowPreviousOnestopIds {
 			sub := sq.StatementBuilder.
-				Select("feed_version_route_onestop_ids.onestop_id", "feed_version_route_onestop_ids.entity_id", "feed_versions.feed_id").
+				Select(
+					"feed_version_route_onestop_ids.onestop_id",
+					"feed_version_route_onestop_ids.entity_id",
+					"feed_versions.feed_id",
+				).
 				Distinct().Options("on (feed_version_route_onestop_ids.onestop_id, feed_version_route_onestop_ids.entity_id, feed_versions.feed_id)").
 				From("feed_version_route_onestop_ids").
 				Join("feed_versions on feed_versions.id = feed_version_route_onestop_ids.feed_version_id").
@@ -273,28 +275,6 @@ func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActiv
 				q = q.Where(sq.Eq{"scount.route_id": nil})
 			}
 		}
-		if where.Bbox != nil {
-			q = q.JoinClause(`JOIN (
-				SELECT DISTINCT ON (tlrs.route_id) tlrs.route_id FROM gtfs_stops
-				JOIN tl_route_stops tlrs ON gtfs_stops.id = tlrs.stop_id
-				WHERE ST_Intersects(gtfs_stops.geometry, ST_MakeEnvelope(?,?,?,?,4326))
-			) tlrs_bbox on tlrs_bbox.route_id = gtfs_routes.id`, where.Bbox.MinLon, where.Bbox.MinLat, where.Bbox.MaxLon, where.Bbox.MaxLat)
-		}
-		if where.Within != nil && where.Within.Valid {
-			q = q.JoinClause(`JOIN (
-				SELECT DISTINCT ON (tlrs.route_id) tlrs.route_id FROM gtfs_stops
-				JOIN tl_route_stops tlrs ON gtfs_stops.id = tlrs.stop_id
-				WHERE ST_Intersects(gtfs_stops.geometry, ?)
-			) tlrs_within on tlrs_within.route_id = gtfs_routes.id`, where.Within)
-		}
-		if where.Near != nil {
-			radius := checkFloat(&where.Near.Radius, 0, 1_000_000)
-			q = q.JoinClause(`JOIN (
-				SELECT DISTINCT ON (tlrs.route_id) tlrs.route_id FROM gtfs_stops
-				JOIN tl_route_stops tlrs ON gtfs_stops.id = tlrs.stop_id
-				WHERE ST_DWithin(gtfs_stops.geometry, ST_MakePoint(?,?), ?)
-			) tlrs_near on tlrs_near.route_id = gtfs_routes.id`, where.Near.Lon, where.Near.Lat, radius)
-		}
 		if where.OperatorOnestopID != nil {
 			q = q.
 				Join("gtfs_agencies ON gtfs_agencies.id = gtfs_routes.agency_id").
@@ -310,17 +290,80 @@ func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActiv
 			q = q.Column(rank).Where(wc)
 		}
 	}
-	if useActive != nil && useActive.Active {
+
+	// Handle geom search
+	if where != nil {
+		loc := where.Location
+		if loc == nil {
+			loc = &model.RouteLocationFilter{
+				Bbox:    where.Bbox,
+				Near:    where.Near,
+				Polygon: where.Within,
+			}
+		}
+		if loc.Bbox != nil {
+			q = q.JoinClause(`JOIN (
+				SELECT DISTINCT ON (tlrs.route_id) tlrs.route_id FROM gtfs_stops
+				JOIN tl_route_stops tlrs ON gtfs_stops.id = tlrs.stop_id
+				WHERE ST_Intersects(gtfs_stops.geometry, ST_MakeEnvelope(?,?,?,?,4326))
+			) tlrs_bbox on tlrs_bbox.route_id = gtfs_routes.id`, loc.Bbox.MinLon, loc.Bbox.MinLat, loc.Bbox.MaxLon, loc.Bbox.MaxLat)
+		}
+		if loc.Polygon != nil && loc.Polygon.Valid {
+			q = q.JoinClause(`JOIN (
+				SELECT DISTINCT ON (tlrs.route_id) tlrs.route_id FROM gtfs_stops
+				JOIN tl_route_stops tlrs ON gtfs_stops.id = tlrs.stop_id
+				WHERE ST_Intersects(gtfs_stops.geometry, ?)
+			) tlrs_within on tlrs_within.route_id = gtfs_routes.id`, loc.Polygon)
+		}
+		if loc.Near != nil {
+			radius := checkFloat(&loc.Near.Radius, 0, 1_000_000)
+			q = q.JoinClause(`JOIN (
+				SELECT DISTINCT ON (tlrs.route_id) tlrs.route_id FROM gtfs_stops
+				JOIN tl_route_stops tlrs ON gtfs_stops.id = tlrs.stop_id
+				WHERE ST_DWithin(gtfs_stops.geometry, ST_MakePoint(?,?), ?)
+			) tlrs_near on tlrs_near.route_id = gtfs_routes.id`, loc.Near.Lon, loc.Near.Lat, radius)
+		}
+		if loc.Focus != nil {
+			// Use materialized route geometries when requested
+			orderExpr := sq.Expr(
+				useActive.UseTable(
+					"tl_route_geometries.geometry <-> ST_MakePoint(?,?), gtfs_routes.id",
+					"gtfs_routes.geometry_simplified <-> ST_MakePoint(?,?), gtfs_routes.id",
+				),
+				loc.Focus.Lon,
+				loc.Focus.Lat)
+			q = q.
+				Join("tl_route_geometries ON tl_route_geometries.route_id = gtfs_routes.id").
+				OrderByClause(orderExpr)
+		}
+	}
+
+	if useActive.Active() {
 		q = q.Join("feed_states on feed_states.feed_version_id = gtfs_routes.feed_version_id")
 	}
 	if len(ids) > 0 {
 		q = q.Where(In("gtfs_routes.id", ids))
 	}
 
+	// Default ordering
+	q = q.OrderBy("gtfs_routes.feed_version_id,gtfs_routes.id")
+
 	// Handle cursor
 	if after != nil && after.Valid && after.ID > 0 {
-		// first check helps improve query performance
-		if after.FeedVersionID == 0 {
+		if where != nil && where.Location != nil && where.Location.Focus != nil {
+			// When materialized, we have tl_materialized_active_routes set as 'gtfs_routes', which has a 'geometry_simplified' column
+			whereExpr := sq.Expr(
+				useActive.UseTable(
+					"(ST_Distance(tl_route_geometries.geometry, ST_MakePoint(?,?)), gtfs_routes.id) > (select ST_Distance(geometry, ST_MakePoint(?,?)), route_id from tl_route_geometries where route_id = ?)",
+					"(ST_Distance(gtfs_routes.geometry_simplified, ST_MakePoint(?,?)), gtfs_routes.id) > (select ST_Distance(geometry, ST_MakePoint(?,?)), route_id from gtfs_routes where route_id = ?)",
+				),
+				where.Location.Focus.Lon,
+				where.Location.Focus.Lat,
+				where.Location.Focus.Lon,
+				where.Location.Focus.Lat,
+				after.ID)
+			q = q.Where(whereExpr)
+		} else if after.FeedVersionID == 0 {
 			q = q.
 				Where(sq.Expr("gtfs_routes.feed_version_id >= (select feed_version_id from gtfs_routes where id = ?)", after.ID)).
 				Where(sq.Expr("(gtfs_routes.feed_version_id, gtfs_routes.id) > (select feed_version_id,id from gtfs_routes where id = ?)", after.ID))

server/gql/route_resolver_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/interline-io/transitland-lib/internal/testconfig"
 	"github.com/interline-io/transitland-lib/server/model"
+	"github.com/interline-io/transitland-lib/tlxy"
 	"github.com/stretchr/testify/assert"
 	"github.com/tidwall/gjson"
 )
@@ -157,53 +158,7 @@ func TestRouteResolver(t *testing.T) {
 			selector:     "routes.#.route_id",
 			selectExpect: []string{"Bu-130"},
 		},
-		// just ensure geometry queries complete successfully; checking coordinates is a pain and flaky.
-		{
-			name:         "where near 100m",
-			query:        `query {routes(where:{near:{lon:-122.407974,lat:37.784471,radius:100.0}}) {route_id route_long_name}}`,
-			selector:     "routes.#.route_id",
-			selectExpect: []string{"01", "05", "07", "11"},
-		},
-		{
-			name:         "where near 10000m",
-			query:        `query {routes(where:{near:{lon:-122.407974,lat:37.784471,radius:10000.0}}) {route_id route_long_name}}`,
-			selector:     "routes.#.route_id",
-			selectExpect: []string{"Bu-130", "Li-130", "Lo-130", "Gi-130", "Sp-130", "01", "05", "07", "11"},
-		},
-		{
-			name:         "where within polygon",
-			query:        `query{routes(where:{within:{type:"Polygon",coordinates:[[[-122.396,37.8],[-122.408,37.79],[-122.393,37.778],[-122.38,37.787],[-122.396,37.8]]]}}){id route_id}}`,
-			selector:     "routes.#.route_id",
-			selectExpect: []string{"01", "05", "07", "11"},
-		},
-		{
-			name:         "where within polygon big",
-			query:        `query{routes(where:{within:{type:"Polygon",coordinates:[[[-122.39481925964355,37.80151060070086],[-122.41653442382812,37.78652126637423],[-122.39662170410156,37.76847577247014],[-122.37301826477051,37.784757615348575],[-122.39481925964355,37.80151060070086]]]}}){id route_id}}`,
-			selector:     "routes.#.route_id",
-			selectExpect: []string{"Bu-130", "Li-130", "Lo-130", "Gi-130", "Sp-130", "01", "05", "07", "11"},
-		},
-		{
-			name:         "where bbox 1",
-			query:        `query($bbox:BoundingBox) {routes(where:{bbox:$bbox}) {route_id route_long_name}}`,
-			vars:         hw{"bbox": hw{"min_lon": -122.2698781543005, "min_lat": 37.80700393130445, "max_lon": -122.2677640139239, "max_lat": 37.8088734037938}},
-			selector:     "routes.#.route_id",
-			selectExpect: []string{"01", "03", "07"},
-		},
-		{
-			name:         "where bbox 2",
-			query:        `query($bbox:BoundingBox) {routes(where:{bbox:$bbox}) {route_id route_long_name}}`,
-			vars:         hw{"bbox": hw{"min_lon": -124.3340029563042, "min_lat": 40.65505368922123, "max_lon": -123.9653594784379, "max_lat": 40.896440342606525}},
-			selector:     "routes.#.route_id",
-			selectExpect: []string{},
-		},
-		{
-			name:        "where bbox too large",
-			query:       `query($bbox:BoundingBox) {routes(where:{bbox:$bbox}) {route_id route_long_name}}`,
-			vars:        hw{"bbox": hw{"min_lon": -137.88020156441956, "min_lat": 30.072648315782004, "max_lon": -109.00421121090919, "max_lat": 45.02437957865729}},
-			expectError: true,
-			f: func(t *testing.T, jj string) {
-			},
-		},
+
 		// route patterns
 		{
 			name: "route patterns",
@@ -286,6 +241,125 @@ func TestRouteResolver(t *testing.T) {
 		// TODO: census_geographies
 	}
 	c, _ := newTestClient(t)
+	queryTestcases(t, c, testcases)
+}
+
+func TestRouteResolver_Location(t *testing.T) {
+	c, cfg := newTestClient(t)
+
+	// Florida coordinates: approximately in the center of Tampa Bay area
+	// This should put HA stops (Tampa area) much closer than BA/CT stops (San Francisco Bay area)
+	floridaFocus := tlxy.Point{Lat: 27.9506, Lon: -82.4572}
+
+	// San Jose coordinates: approximately in downtown San Jose
+	// This should put CT stops (Caltrain San Jose area) much closer than HA stops (Florida)
+	sanJoseFocus := tlxy.Point{Lat: 37.3382, Lon: -121.8863}
+	var testRouteId int
+	if err := cfg.Finder.DBX().
+		QueryRowx(`select gtfs_routes.id from gtfs_routes join feed_states using(feed_version_id) join current_feeds cf on cf.id = feed_states.feed_id where cf.onestop_id = $1 and route_id = $2`, "HA", "96").
+		Scan(&testRouteId); err != nil {
+		t.Errorf("could not get route ID for test: %s", err.Error())
+	}
+
+	testcases := []testcase{
+		// just ensure geometry queries complete successfully; checking coordinates is a pain and flaky.
+		{
+			name:         "where near 100m",
+			query:        `query {routes(where:{near:{lon:-122.407974,lat:37.784471,radius:100.0}}) {route_id route_long_name}}`,
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"01", "05", "07", "11"},
+		},
+		{
+			name:         "where near 10000m",
+			query:        `query {routes(where:{near:{lon:-122.407974,lat:37.784471,radius:10000.0}}) {route_id route_long_name}}`,
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"Bu-130", "Li-130", "Lo-130", "Gi-130", "Sp-130", "01", "05", "07", "11"},
+		},
+		{
+			name:         "where within polygon",
+			query:        `query{routes(where:{within:{type:"Polygon",coordinates:[[[-122.396,37.8],[-122.408,37.79],[-122.393,37.778],[-122.38,37.787],[-122.396,37.8]]]}}){id route_id}}`,
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"01", "05", "07", "11"},
+		},
+		{
+			name:         "where within polygon big",
+			query:        `query{routes(where:{within:{type:"Polygon",coordinates:[[[-122.39481925964355,37.80151060070086],[-122.41653442382812,37.78652126637423],[-122.39662170410156,37.76847577247014],[-122.37301826477051,37.784757615348575],[-122.39481925964355,37.80151060070086]]]}}){id route_id}}`,
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"Bu-130", "Li-130", "Lo-130", "Gi-130", "Sp-130", "01", "05", "07", "11"},
+		},
+		{
+			name:         "where bbox 1",
+			query:        `query($bbox:BoundingBox) {routes(where:{bbox:$bbox}) {route_id route_long_name}}`,
+			vars:         hw{"bbox": hw{"min_lon": -122.2698781543005, "min_lat": 37.80700393130445, "max_lon": -122.2677640139239, "max_lat": 37.8088734037938}},
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"01", "03", "07"},
+		},
+		{
+			name:         "where bbox 2",
+			query:        `query($bbox:BoundingBox) {routes(where:{bbox:$bbox}) {route_id route_long_name}}`,
+			vars:         hw{"bbox": hw{"min_lon": -124.3340029563042, "min_lat": 40.65505368922123, "max_lon": -123.9653594784379, "max_lat": 40.896440342606525}},
+			selector:     "routes.#.route_id",
+			selectExpect: []string{},
+		},
+		{
+			name:        "where bbox too large",
+			query:       `query($bbox:BoundingBox) {routes(where:{bbox:$bbox}) {route_id route_long_name}}`,
+			vars:        hw{"bbox": hw{"min_lon": -137.88020156441956, "min_lat": 30.072648315782004, "max_lon": -109.00421121090919, "max_lat": 45.02437957865729}},
+			expectError: true,
+			f: func(t *testing.T, jj string) {
+			},
+		},
+		// Focus test cases
+		{
+			name: "focus basic: Florida focus point returns HA routes first",
+			query: `query($lat:Float!, $lon:Float!) {
+				routes(limit: 5, where: {location: {focus: {lat: $lat, lon: $lon}}}) {
+					route_id
+					feed_version { feed { onestop_id } }
+				}
+			}`,
+			vars:         hw{"lat": floridaFocus.Lat, "lon": floridaFocus.Lon},
+			selector:     "routes.#.feed_version.feed.onestop_id",
+			selectExpect: []string{"HA", "HA", "HA", "HA", "HA"},
+		},
+		{
+			name: "focus basic: San Jose focus point returns West Coast routes first",
+			query: `query($lat:Float!, $lon:Float!) {
+				routes(limit: 5, where: {location: {focus: {lat: $lat, lon: $lon}}}) {
+					route_id
+					feed_version { feed { onestop_id } }
+				}
+			}`,
+			vars:         hw{"lat": sanJoseFocus.Lat, "lon": sanJoseFocus.Lon},
+			selector:     "routes.#.feed_version.feed.onestop_id",
+			selectExpect: []string{"CT", "CT", "CT", "CT", "CT"},
+		},
+		{
+			name: "focus with feed filter: HA routes only, ordered by distance",
+			query: `query($lat:Float!, $lon:Float!) {
+				routes(limit: 10, where: {feed_onestop_id: "HA", location: {focus: {lat: $lat, lon: $lon}}}) {
+					route_id
+					geometry
+				}
+			}`,
+			vars:         hw{"lat": floridaFocus.Lat, "lon": floridaFocus.Lon},
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"20", "51", "8", "400", "96", "97", "12", "9", "19", "30"},
+		},
+		{
+			// Should start after "96" in above test
+			name: "focus with pagination",
+			query: `query($lat:Float!, $lon:Float!, $after: Int!) {
+				routes(after:$after,limit: 10, where: {feed_onestop_id: "HA", location: {focus: {lat: $lat, lon: $lon}}}) {
+					route_id
+					geometry
+				}
+			}`,
+			vars:         hw{"lat": floridaFocus.Lat, "lon": floridaFocus.Lon, "after": testRouteId},
+			selector:     "routes.#.route_id",
+			selectExpect: []string{"97", "12", "9", "19", "30", "60", "7", "24", "25", "275"},
+		},
+	}
 	queryTestcases(t, c, testcases)
 }
 

ext/builders/route_headway_builder.go
@@ -121,9 +121,10 @@ func (pp *RouteHeadwayBuilder) Copy(copier adapters.EntityCopier) error {
 			d, _ := time.Parse("2006-01-02", day)
 			dow := d.Weekday()
 			dowCat := 1
-			if dow == time.Saturday {
+			switch dow {
+			case time.Saturday:
 				dowCat = 6
-			} else if dow == time.Sunday {
+			case time.Sunday:
 				dowCat = 7
 			}
 			if _, ok := dowCatDay[dowCat]; !ok {

commit 0dcf46b24313c273bd6669429312d6c89ec4cdf4

Use tl_route_representative_shapes in queries (#701)
## Summary
Migrates every reader of `tl_route_geometries` to reconstruct route
geometry on the fly from `tl_route_representative_shapes` (added in
#694), so `tl_route_geometries` becomes write-only and can be dropped in
a follow-up. That table stores one ranked pointer per route to the
`gtfs_shapes` it was assembled from — rank 0 is the primary line, the
ranked set is the combined geometry, and the route-level scalars are
recoverable as aggregates — so the reconstruction is exact and
transparent to callers. No schema, model, or GraphQL changes.

### Route geometry resolver
`RouteGeometriesByRouteIDs`, which serves both `route.geometry` and
`route.geometries`, now `GROUP BY route_id` over
`tl_route_representative_shapes` joined to `gtfs_shapes`:
`ST_Multi(ST_Collect(... ORDER BY rank))` for `combined_geometry`, the
rank-0 shape for the primary `geometry`, and `max`/`bool_or` for
`length` / `max_segment_length` / `first_point_max_distance` /
`generated`. The model and resolver signatures are unchanged.

### Nearest-route (focus) sort
The `location.focus` KNN sort's non-materialized branch previously
sorted against `tl_route_geometries.geometry`; it now sorts against the
route's rank-0 representative shape, in both the order-by and the keyset
cursor. The materialized branch is unchanged — it continues to use the
indexed `gtfs_routes.geometry_simplified`, so the hot path is
unaffected.

### Materialized routes
The materialization that builds `tl_materialized_active_routes` derived
`geometry_simplified` from `tl_route_geometries.geometry`; it now
derives it from the rank-0 representative shape
(`ST_Simplify(ST_Force2D(...))`). Because `ST_Simplify` is sensitive to
the sub-meter difference between the old blob and the raw shape
coordinates, re-materialized `geometry_simplified` values shift slightly
— cosmetic for nearest-route ranking, but they are not byte-identical to
before.

### Incidental fix: materialized focus-pagination cursor
Unrelated to representative shapes, but surfaced while reworking the
focus cursor: the *materialized* branch of the keyset predicate had a
pre-existing bug (introduced in #520). Its subquery read `from

internal/feedstate/manager.go
@@ -254,10 +254,11 @@ func routeMaterializeFields(spatial bool) map[string]string {
 		"created_at":          "gtfs_routes.created_at",
 		"updated_at":          "gtfs_routes.updated_at",
 	}
-	// Geometry column - full geometry for SQLite, simplified for PostGIS
-	fields["geometry_simplified"] = "tlrg.geometry"
+	// Geometry column - full geometry for SQLite, simplified for PostGIS.
+	// Sourced from the route's primary (rank 0) representative shape.
+	fields["geometry_simplified"] = "rep_shape.geometry"
 	if spatial {
-		fields["geometry_simplified"] = "ST_Simplify(tlrg.geometry::geometry, 0.01)"
+		fields["geometry_simplified"] = "ST_Simplify(ST_Force2D(rep_shape.geometry::geometry), 0.01)"
 	}
 	return fields
 }
@@ -351,7 +352,8 @@ func (m *Manager) MaterializeFeedVersion(ctx context.Context, feedVersionID int)
 			Join("gtfs_agencies ON gtfs_agencies.id = gtfs_routes.agency_id").
 			Join("feed_versions ON feed_versions.id = gtfs_routes.feed_version_id").
 			LeftJoin("feed_version_route_onestop_ids osid ON osid.entity_id = gtfs_routes.route_id AND osid.feed_version_id = feed_versions.id").
-			LeftJoin("tl_route_geometries tlrg ON tlrg.route_id = gtfs_routes.id").
+			LeftJoin("tl_route_representative_shapes rep0 ON rep0.route_id = gtfs_routes.id AND rep0.rank = 0").
+			LeftJoin("gtfs_shapes rep_shape ON rep_shape.id = rep0.shape_id").
 			Where(sq.Eq{"gtfs_routes.feed_version_id": feedVersionID}))
 
 	_, err := routeQuery.ExecContext(ctx)

server/finders/dbfinder/route.go
@@ -101,20 +101,27 @@ func (f *Finder) RouteStopPatternsByRouteIDs(ctx context.Context, limit *int, ke
 	return arrangeGroup(keys, ents, func(ent *model.RouteStopPattern) int { return ent.RouteID }), err
 }
 
+// RouteGeometriesByRouteIDs reconstructs each route's geometry from the shapes it
+// points at in tl_route_representative_shapes: rank 0 is the primary line, the ranked
+// set collected is the combined geometry, and the scalars are aggregates over the
+// pointed-at shapes. There is one geometry per route, so limit is unused.
 func (f *Finder) RouteGeometriesByRouteIDs(ctx context.Context, limit *int, keys []int) ([][]*model.RouteGeometry, error) {
 	var ents []*model.RouteGeometry
-	err := dbutil.Select(ctx,
-		f.db,
-		lateralWrap(
-			quickSelectOrder("tl_route_geometries", limit, nil, nil, "route_id"),
-			"gtfs_routes",
-			"id",
-			"tl_route_geometries",
-			"route_id",
-			keys,
-		),
-		&ents,
-	)
+	q := sq.StatementBuilder.
+		Select(
+			"tl_route_representative_shapes.route_id AS route_id",
+			"bool_or(tl_route_representative_shapes.generated) AS generated",
+			"max(tl_route_representative_shapes.length) AS length",
+			"max(tl_route_representative_shapes.max_segment_length) AS max_segment_length",
+			"max(tl_route_representative_shapes.first_point_max_distance) AS first_point_max_distance",
+			"ST_Force2D((array_agg(gtfs_shapes.geometry::geometry ORDER BY tl_route_representative_shapes.rank))[1]) AS geometry",
+			"ST_Multi(ST_Collect(ST_Force2D(gtfs_shapes.geometry::geometry) ORDER BY tl_route_representative_shapes.rank)) AS combined_geometry",
+		).
+		From("tl_route_representative_shapes").
+		Join("gtfs_shapes ON gtfs_shapes.id = tl_route_representative_shapes.shape_id").
+		Where(In("tl_route_representative_shapes.route_id", keys)).
+		GroupBy("tl_route_representative_shapes.route_id")
+	err := dbutil.Select(ctx, f.db, q, &ents)
 	return arrangeGroup(keys, ents, func(ent *model.RouteGeometry) int { return ent.RouteID }), err
 }
 
@@ -334,16 +341,18 @@ func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActiv
 			) tlrs_near on tlrs_near.route_id = gtfs_routes.id`, loc.Near.Lon, loc.Near.Lat, radius)
 		}
 		if loc.Focus != nil {
-			// Use materialized route geometries when requested
+			// Materialized routes carry geometry_simplified; otherwise sort against the
+			// route's primary (rank 0) representative shape.
 			orderExpr := sq.Expr(
 				useActive.UseTable(
-					"tl_route_geometries.geometry <-> ST_MakePoint(?,?), gtfs_routes.id",
+					"rep_shape.geometry <-> ST_MakePoint(?,?), gtfs_routes.id",
 					"gtfs_routes.geometry_simplified <-> ST_MakePoint(?,?), gtfs_routes.id",
 				),
 				loc.Focus.Lon,
 				loc.Focus.Lat)
 			q = q.
-				Join("tl_route_geometries ON tl_route_geometries.route_id = gtfs_routes.id").
+				Join("tl_route_representative_shapes rep0 ON rep0.route_id = gtfs_routes.id AND rep0.rank = 0").
+				Join("gtfs_shapes rep_shape ON rep_shape.id = rep0.shape_id").
 				OrderByClause(orderExpr)
 		}
 	}
@@ -364,8 +373,8 @@ func routeSelect(limit *int, after *model.Cursor, ids []int, useActive *UseActiv
 			// When materialized, we have tl_materialized_active_routes set as 'gtfs_routes', which has a 'geometry_simplified' column
 			whereExpr := sq.Expr(
 				useActive.UseTable(
-					"(ST_Distance(tl_route_geometries.geometry, ST_MakePoint(?,?)), gtfs_routes.id) > (select ST_Distance(geometry, ST_MakePoint(?,?)), route_id from tl_route_geometries where route_id = ?)",
-					"(ST_Distance(gtfs_routes.geometry_simplified, ST_MakePoint(?,?)), gtfs_routes.id) > (select ST_Distance(geometry, ST_MakePoint(?,?)), route_id from gtfs_routes where route_id = ?)",
+					"(ST_Distance(rep_shape.geometry, ST_MakePoint(?,?)), gtfs_routes.id) > (select ST_Distance(s.geometry, ST_MakePoint(?,?)), r.route_id from tl_route_representative_shapes r join gtfs_shapes s on s.id = r.shape_id where r.route_id = ? and r.rank = 0)",
+					"(ST_Distance(gtfs_routes.geometry_simplified, ST_MakePoint(?,?)), gtfs_routes.id) > (select ST_Distance(geometry_simplified, ST_MakePoint(?,?)), id from tl_materialized_active_routes where id = ?)",
 				),
 				where.Location.Focus.Lon,
 				where.Location.Focus.Lat,

server/finders/dbfinder/route_test.go
@@ -1,0 +1,36 @@
+package dbfinder
+
+import (
+	"context"
+	"testing"
+
+	"github.com/interline-io/transitland-lib/server/dbutil"
+	"github.com/interline-io/transitland-lib/server/model"
+	"github.com/interline-io/transitland-lib/server/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+// Exercises the active+materialized focus-pagination path, which had no coverage --
+// letting a broken cursor subquery (text route_id vs int id, and a missing geometry
+// column) survive. Asserts only that the query executes: it is the first test to run
+// routeSelect in materialized mode.
+func TestRouteSelect_MaterializedFocusCursor(t *testing.T) {
+	ctx := context.Background()
+	db := testutil.MustOpenTestDB(t)
+
+	var afterID int
+	if err := db.Get(&afterID, "select id from tl_materialized_active_routes limit 1"); err != nil {
+		t.Skipf("no materialized routes in test db: %v", err)
+	}
+
+	q := routeSelect(
+		nil,
+		&model.Cursor{Valid: true, ID: afterID},
+		nil,
+		&UseActive{active: true, materialized: true},
+		&model.PermFilter{IsGlobalAdmin: true},
+		&model.RouteFilter{Location: &model.RouteLocationFilter{Focus: &model.FocusPoint{Lon: -122.4, Lat: 37.8}}},
+	)
+	var ents []*model.Route
+	require.NoError(t, dbutil.Select(ctx, db, q, &ents))
+}

server/gql/route_resolver_test.go
@@ -69,6 +69,29 @@ func TestRouteResolver(t *testing.T) {
 			selectExpect: []string{"false"},
 		},
 		{
+			// Migration guard: geometry is reconstructed from tl_route_representative_shapes,
+			// so pin the structural invariants on a reviewed shape (Caltrain Bu-130). These
+			// hold regardless of the source table and outlive the tl_route_geometries drop.
+			name:  "geometry invariants (Bu-130)",
+			query: `query { routes(where:{route_id:"Bu-130"}) { geometries { generated geometry combined_geometry } } }`,
+			f: func(t *testing.T, jj string) {
+				g := gjson.Get(jj, "routes.0.geometries.0")
+				// combined_geometry is a MultiLineString with one linestring per representative shape.
+				assert.Equal(t, "MultiLineString", g.Get("combined_geometry.type").String())
+				assert.Len(t, g.Get("combined_geometry.coordinates").Array(), 4, "combined_geometry linestring count")
+				// primary geometry is the rank-0 line: a LineString equal to combined_geometry[0].
+				assert.Equal(t, "LineString", g.Get("geometry.type").String())
+				assert.Equal(t, g.Get("combined_geometry.coordinates.0").Raw, g.Get("geometry.coordinates").Raw, "primary geometry == combined_geometry[0]")
+				assert.False(t, g.Get("generated").Bool())
+				// shape_id isn't exposed via the resolver, so pin the rank-0 line's point
+				// count and first vertex to lock the actual shape it resolves to.
+				pts := g.Get("geometry.coordinates").Array()
+				assert.Len(t, pts, 382, "primary geometry point count")
+				assert.InDelta(t, -121.903170, pts[0].Array()[0].Float(), 1e-5, "first vertex lon")
+				assert.InDelta(t, 37.330157, pts[0].Array()[1].Float(), 1e-5, "first vertex lat")
+			},
+		},
+		{
 			name:         "route_stop_buffer stop_points 10m",
 			query:        `query($route_id: String!) { routes(where:{route_id:$route_id}) {route_stop_buffer(radius: 100.0) {stop_points	stop_buffer	stop_convexhull}}}`,
 			vars:         vars,